d498924dcb3cb4f0b2ff06f96d018940ea81acaa
David Paul Graham <dpgraham4401@gmail.com>
2022-03-29 16:48:09 -0400

    initial logout and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 from django.db import IntegrityError
 
 
-def signup(request):
+def signup_haztrak(request):
     if request.method == 'GET':
         return render(request, 'accounts/signup.html',
                       {'form': UserCreationForm})
@@ -24,6 +25,27 @@ def signup(request):
         else:
             return render(request, 'accounts/signup.html',
                           {'form': UserCreationForm, 'error': 'Passwords do not match'})
+
+
+def logout_haztrak(request):
+    logout(request)
+    return redirect('home')
+
+
+def login_haztrak(request):
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html',
+                      {'form': AuthenticationForm})
+    else:
+        user = authenticate(request, username=request.POST['username'],
+                            password=request.POST['password'])
+        if user is None:
+            return render(request, 'accounts/login.html',
+                          {'form': AuthenticationForm,
+                           'error': 'Login failed, check your username and password.'})
+        else:
+            login(request, user)
+            return redirect('home')
 
 
 def profile(request):
