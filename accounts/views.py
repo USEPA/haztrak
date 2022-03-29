2aa7752845f9530f6e6b6f26ccde0286b79e2296
David Paul Graham <dpgraham4401@gmail.com>
2022-03-29 15:58:55 -0400

    initial user signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.db import IntegrityError
 
 
 def signup(request):
-    return render(request, 'accounts/signup.html',
-                  {'form':UserCreationForm})
+    if request.method == 'GET':
+        return render(request, 'accounts/signup.html',
+                      {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'accounts/signup.html',
+                              {'form': UserCreationForm, 'error': 'Username exists. Choose a new username'})
+        else:
+            return render(request, 'accounts/signup.html',
+                          {'form': UserCreationForm, 'error': 'Passwords do not match'})
 
 
 def profile(request):
