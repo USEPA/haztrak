1c5e2f2197590648613657fd6fae58ae58ba293b
David Paul Graham <dpgraham4401@gmail.com>
2022-04-01 08:43:44 -0400

    initial manifest view page

--- trak/views.py
+++ trak/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .rcrainfo.manifest import sync_mtn
 from .models import Manifest
 
 
-# Create your views here.
 def trak_home(request):
     manifests = Manifest.objects.filter()
     return render(request, 'trak/trak.html', {'manifests': manifests})
+
+
+def manifest_view(request, manifest_id):
+    manifest = get_object_or_404(Manifest, pk=manifest_id)
+    return render(request, 'trak/manifest.html', {'manifest': manifest})
 
 
 def sync(request, num):
