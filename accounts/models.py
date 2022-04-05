b4374e973745dc647e226cceeb9b7d0137cde1d3
David Paul Graham <dpgraham4401@gmail.com>
2022-04-05 17:48:26 -0400

    add epa_site_id ManyToManyField to profile, shows which sites user has access to

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
+from sites.models import EpaSite
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     rcra_api_key = models.CharField(max_length=128)
     rcra_api_id = models.CharField(max_length=128)
-    # avatar = models.ImageField(default='default_profile.jpg', upload_to='profile_images')
+    epa_sites = models.ManyToManyField(EpaSite)
 
     def __str__(self):
         return self.user.username
--- sites/views.py
+++ sites/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from .models import EpaSite
 
 
 def sites_dashboard(request):
-    return render(request, 'sites/sites.html')
+    # sites_with_access = request.user.profile.epa_sites.filter().get()
+    epa_sites = request.user.profile.epa_sites.all()
+    # epa_sites = EpaSite.objects.filter(epa_id='VATESTGEN001')
+    print(type(epa_sites))
+    return render(request, 'sites/sites.html', {'sites': epa_sites})
