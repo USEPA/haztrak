f909c60c3a2983e4288a020c5fdad63a969d6b7c
dpgraham4401 <dpgraham4401@gmail.com>
2022-04-04 21:06:07 -0400

    setup fake postgres

--- haztrak/settings.py
+++ haztrak/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'haztrak.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'helloworld',
+        'USER': 'dg',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
