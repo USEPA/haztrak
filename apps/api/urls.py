2e73a74b2376da2b6a2bc8ef41c7cbd00ba63033
David Paul Graham <dpgraham4401@gmail.com>
2022-06-16 13:01:49 -0500

    initial PullManifest APIView with post method

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('manifest/<str:mtn>', views.ManifestView.as_view()),
     # site
     path('handler/<str:epa_id>', views.HandlerView.as_view()),
+    # rcrainfo integration actions
+    path('rcrainfo/manifest-pull', views.PullManifest.as_view()),
     # other
     path('sync/<str:site_id>', views.SyncSiteManifest.as_view(), name='sync'),
     path('tran/<int:tran_id>', views.TransporterView.as_view())
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,11 +1,15 @@
 import http
+import os
 
 from django.core.exceptions import ObjectDoesNotExist
+from emanifest import client as em
+from rest_framework import status
 from rest_framework.exceptions import APIException
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from apps.accounts.models import Profile
 from apps.trak.models import Handler, Manifest, Transporter
 from lib.rcrainfo import rcrainfo
 
@@ -35,7 +39,7 @@ class ManifestView(APIView):
 
     def post(self, request: Request, mtn: str = None) -> Response:
         if not mtn:
-            return self.response(status=400)
+            return self.response(status=status.HTTP_400_BAD_REQUEST)
         else:
             serializer = ManifestSerializer(data=request.data)
             valid = serializer.is_valid()
@@ -46,7 +50,7 @@ class ManifestView(APIView):
                 return self.response(status=500)
 
 
-# trash to be fixed, can't be bothered to remove it right now
+# trash, not time remove it right now
 class SyncSiteManifest(APIView):
     response = Response
 
@@ -55,7 +59,7 @@ class SyncSiteManifest(APIView):
             resp = rcrainfo.get_mtns(epa_id)
             return Response(data={'mtn': resp.json})
         else:
-            return self.response(status=200)
+            return self.response(status=status.HTTP_200_OK)
 
 
 class HandlerView(APIView):
@@ -88,3 +92,25 @@ class TransporterView(APIView):
         except ObjectDoesNotExist:
             return self.response(status=http.HTTPStatus.NOT_FOUND,
                                  data={'Error': f'{tran_id} not found'})
+
+
+# ToDo authentication, right now will work as long as user is signed
+#  into haztrak via the browser for demo purposes. Just Demo!
+class PullManifest(APIView):
+    if os.getenv('RCRAINFO_ENV'):
+        ri_client = em.new_client(os.getenv('RCRAINFO_ENV'))
+    else:
+        ri_client = em.new_client('preprod')
+
+    def post(self, request: Request) -> Response:
+        try:
+            user_profile = Profile.objects.get(user_id=self.request.user)
+            self.ri_client.Auth(user_profile.rcra_api_id, user_profile.rcra_api_key)
+            data = {'mtn': []}
+            for mtn in self.request.data['mtn']:
+                resp = self.ri_client.GetManByMTN(mtn)
+                data['mtn'].append({mtn: resp.response.status_code})
+            print(data)
+            return Response(status=status.HTTP_200_OK, data=data)
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
