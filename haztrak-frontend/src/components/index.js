c0449f3f9eda62e6f6b020e47a0487b01a607942
David Paul Graham <dpgraham4401@gmail.com>
2022-06-30 14:40:20 -0400

    merge new changes

--- haztrak-frontend/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import AppHeader from './AppHeader'
-import AppHeaderDropdown from './header/AppHeaderDropdown'
-import AppSidebar from './AppSidebar'
-import DocsCallout from './DocsCallout'
-import DocsLink from './DocsLink'
-import DocsExample from './DocsExample'
-
-export { AppHeader, AppHeaderDropdown, AppSidebar, DocsCallout, DocsLink, DocsExample }
--- haztrak-frontend/src/index.js
+++ haztrak-frontend/src/index.js
@@ -1,18 +1,12 @@
-import 'react-app-polyfill/stable'
-import 'core-js'
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { Provider } from 'react-redux'
-import store from './store'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App/>
 )
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
