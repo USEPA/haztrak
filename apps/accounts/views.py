cfb77abbbe349221b28f8e494459e93be25bb814
David Paul Graham <dpgraham4401@gmail.com>
2022-07-09 21:28:16 -0400

    remove old Auth views

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,63 +1,11 @@
 from django.contrib import messages
-from django.contrib.auth import (authenticate, login, logout,
-                                 update_session_auth_hash)
+from django.contrib.auth import (update_session_auth_hash)
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import (AuthenticationForm, PasswordChangeForm,
-                                       UserCreationForm)
-from django.contrib.auth.models import User
-from django.db import IntegrityError
+from django.contrib.auth.forms import (PasswordChangeForm)
 from django.shortcuts import redirect, render
 
 from .forms import UpdateProfileForm, UpdateUserForm
 from .models import Profile
-
-
-def signup_haztrak(request):
-    if request.method == 'GET':
-        if request.user.is_authenticated:
-            return redirect('home')
-        else:
-            return render(request, 'accounts/signup.html',
-                          {'form': UserCreationForm})
-    else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(request.POST['username'],
-                                                password=request.POST['password1'])
-                user.save()
-                login(request, user)
-                return redirect('home')
-            except IntegrityError:
-                return render(request, 'accounts/signup.html',
-                              {'form': UserCreationForm,
-                               'error': 'Username exists. Choose a new username'})
-        else:
-            return render(request, 'accounts/signup.html',
-                          {'form': UserCreationForm, 'error': 'Passwords do not match'})
-
-
-def logout_haztrak(request):
-    logout(request)
-    return redirect('home')
-
-
-def login_haztrak(request):
-    if request.method == 'GET':
-        if request.user.is_authenticated:
-            return redirect('home')
-        else:
-            return render(request, 'accounts/login.html',
-                          {'form': AuthenticationForm})
-    else:
-        user = authenticate(request, username=request.POST['username'],
-                            password=request.POST['password'])
-        if user is None:
-            return render(request, 'accounts/login.html',
-                          {'form': AuthenticationForm,
-                           'error': 'Login failed, check your username and password.'})
-        else:
-            login(request, user)
-            return redirect('home')
 
 
 @login_required
