7f56428da711552bc59eddcd9f5fcf6140be979a
David Graham <dpgraham4401@gmail.com>
2022-07-13 10:55:20 -0400

    add prettier to project and let it reformat everything (so much easier on the eyes)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,8 +14,8 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <App/>
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
