9bc0307bf6c18b018f0ab1dbf51ce780bc75029a
David Graham <dpgraham4401@gmail.com>
2022-07-13 06:00:11 -0400

    remove my learning slice

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Button } from 'react-bootstrap';
-import { getUser } from '../_store';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Button} from 'react-bootstrap';
+import {getUser} from '../_store';
 
 function Home() {
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.auth);
-  const { on } = useSelector((state) => state.my);
+  const {user} = useSelector((state) => state.auth);
 
   useEffect(() => {
     // get user profile information first time login redirects to home page
@@ -16,18 +15,8 @@ function Home() {
   return (
     <div>
       <h1>
-        Hi
-        {user}
-        !
+        Hi {user}!
       </h1>
-      <Button
-        className={`btn ${on ? 'btn-primary' : 'btn-success'} `}
-        onClick={() => dispatch({ type: 'mySlice/toggleOn' })}
-      >
-        turn
-        {' '}
-        {on ? 'off' : 'on'}
-      </Button>
     </div>
   );
 }
