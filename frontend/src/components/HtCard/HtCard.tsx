5480e770dbfe5aa64b4bdb85a1d06c062f378f22
David Graham <dpgraham4401@gmail.com>
2022-08-10 13:27:17 -0400

    bare minimum typescript configs
    convert HtCard to typescript

--- frontend/src/components/HtCard/HtCard.tsx
+++ frontend/src/components/HtCard/HtCard.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
+
+interface props {
+    children: any;
+}
+
+interface headerProps {
+    title: string;
+    children: any;
+}
+
+interface spinnerProps {
+    message: string;
+}
 
 /**
  * Card with haztrak styling, yeah baby
@@ -13,8 +26,10 @@ import { Card } from 'react-bootstrap';
  *   <HtCard.Footer>submit button here<HtCard.Footer>
  * </HtCard>
  */
-function HtCard(props) {
-  return <div className="m-5 shadow-lg">{props.children}</div>;
+function HtCard(props: props) {
+    return (
+        <div className="m-5 shadow-lg bg-light rounded-2">{props.children}</div>
+    );
 }
 
 /**
@@ -27,15 +42,15 @@ function HtCard(props) {
  *   <HtCard.Header title="Card Title!">{top right dropdown button}<HtCard.Header>
  * </HtCard>
  */
-HtCard.Header = function (props) {
-  return (
-    <Card.Header className="bg-primary text-light">
-      <div className="d-flex justify-content-between">
-        <div className="fw-bold h5">{props.title}</div>
-        <div>{props.children}</div>
-      </div>
-    </Card.Header>
-  );
+HtCard.Header = function (props: headerProps) {
+    return (
+        <Card.Header className="bg-primary text-light rounded-top">
+            <div className="d-flex justify-content-between p-1 px-2 py-1">
+                <div className="fw-bold h5">{props.title}</div>
+                <div>{props.children}</div>
+            </div>
+        </Card.Header>
+    );
 };
 
 /**
@@ -48,12 +63,13 @@ HtCard.Header = function (props) {
  *   <HtCard.Footer>Put button here!<HtCard.Footer>
  * </HtCard>
  */
-HtCard.Footer = function (props) {
-  return (
-    <Card.Footer className="bg-gray">
-      <div className="d-flex justify-content-start gap-2">{props.children}</div>
-    </Card.Footer>
-  );
+HtCard.Footer = function (props: props) {
+    return (
+        <Card.Footer className="bg-gray">
+            <div
+                className="d-flex justify-content-start gap-2">{props.children}</div>
+        </Card.Footer>
+    );
 };
 
 /**
@@ -66,8 +82,8 @@ HtCard.Footer = function (props) {
  *   <HtCard.Body>Hello World!<HtCard.Body>
  * </HtCard>
  */
-HtCard.Body = function (props) {
-  return <Card.Body className="bg-light">{props.children}</Card.Body>;
+HtCard.Body = function (props: props) {
+    return <Card.Body className="bg-light">{props.children}</Card.Body>;
 };
 
 /**
@@ -80,13 +96,15 @@ HtCard.Body = function (props) {
  *   <HtCard.Spinner message="loading..."/>
  * </HtCard.Body>
  */
-HtCard.Spinner = function ({ message }) {
-  return (
-    <h4 className="d-flex justify-content-center text-muted">
-      <i className="fa-solid fa-spin fa-circle-notch"></i>
-      &nbsp;&nbsp;{message}
-    </h4>
-  );
+HtCard.Spinner = function ({message}: spinnerProps) {
+    return (
+        <>
+            <h4 className="d-flex justify-content-center text-muted bg-transparent p-1 py-3">
+                <i className="fa-solid fa-spin fa-circle-notch"></i>
+                &nbsp;&nbsp;{message}
+            </h4>
+        </>
+    );
 };
 
 export default HtCard;
