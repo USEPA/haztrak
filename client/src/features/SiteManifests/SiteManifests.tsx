bf04e3adbc21398ee038868be987caf38c4291b5
David Graham <dpgraham4401@gmail.com>
2022-08-12 11:12:23 -0400

    add HtTooltip.tsx

--- client/src/features/SiteManifests/SiteManifests.tsx
+++ client/src/features/SiteManifests/SiteManifests.tsx
@@ -1,13 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import api from '../../services';
-import { Container, Table } from 'react-bootstrap';
+import {Container, Table} from 'react-bootstrap';
 import HtCard from '../../components/HtCard';
+import HtTooltip from '../../components/HtTooltip';
 
 interface SiteManifest {
   generator: string[];
   transporter: string[];
   tsd: string[];
+}
+
+function manifestTable(manifest: string[]) {
+  return (
+    <Table hover>
+      <thead>
+      <tr>
+        <th>Manifest Tracking Number</th>
+        <th>Status</th>
+        <th></th>
+        <th></th>
+      </tr>
+      </thead>
+      <tbody>
+      {manifest.map((mtn, i) => {
+        return (
+          <tr key={i}>
+            <td>{mtn}</td>
+            <td>status</td>
+            <td>
+              <HtTooltip text={`View: ${mtn}`}>
+                <Link to={`/manifest/${mtn}/view`}>
+                  <i className="fa-solid fa-eye"></i>
+                </Link>
+              </HtTooltip>
+            </td>
+            <td>
+              <HtTooltip text={`Edit ${mtn}`}>
+                <Link to={`/manifest/${mtn}/edit`}>
+                  <i className="fa-solid fa-file-lines"></i>
+                </Link>
+              </HtTooltip>
+            </td>
+          </tr>
+        );
+      })}
+      </tbody>
+    </Table>
+  );
 }
 
 function SiteManifests() {
@@ -27,32 +67,11 @@ function SiteManifests() {
       .then(() => setLoading(false));
   }, [params.siteId]);
 
-  function manifestTable(manifest: string[]) {
-    return (
-      <Table hover>
-        <thead>
-          <tr>
-            <th>Manifest Tracking Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {manifest.map((mtn, i) => {
-            return (
-              <tr key={i}>
-                <td>{mtn}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-    );
-  }
-
   return (
     <>
       <Container>
         <HtCard>
-          <HtCard.Header title="Designated Receiving Facility" />
+          <HtCard.Header title="Designated Receiving Facility"/>
           <HtCard.Body>
             {siteManifest ? (
               manifestTable(siteManifest.tsd)
