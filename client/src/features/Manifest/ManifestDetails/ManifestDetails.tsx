949086e61fe22613f69e94a8c3b331dde52d1d42
David Graham <dpgraham4401@gmail.com>
2022-08-19 16:09:04 -0400

    adjust ManifestDetails.tsx useEffect url to pull manifest

--- client/src/features/Manifest/ManifestDetails/ManifestDetails.tsx
+++ client/src/features/Manifest/ManifestDetails/ManifestDetails.tsx
@@ -3,28 +3,42 @@ import {useParams} from 'react-router-dom';
 import api from '../../../services';
 import {sleepDemo} from '../../../utils/utils';
 
+interface Manifest {
+  manifestTrackingNumber: string;
+}
+
 function ManifestDetails(): JSX.Element {
   let params = useParams();
-  const [manifestData, setManifestData] = useState();
+  const [manifestData, setManifestData] = useState<Manifest | undefined>(
+    undefined
+  );
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
   useEffect(() => {
     setLoading(true);
     api
-      .get(`trak/site/${params.siteId}`, null)
+      .get(`trak/manifest/${params.mtn}`, null)
       .then((response) => {
         // Begin HT Example
         // sleepDemo illustrates how HT handles async hydration
         sleepDemo(750).then(() => setLoading(false));
         // setLoading(false)
         // End HT Example
-        setManifestData(response as any);
+        setManifestData(response as Manifest);
+        console.log(response);
       })
-      .catch(setError);
-  }, [params.siteId]);
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [params.mtn]);
 
-  return <p>Manifest Details</p>;
+  return manifestData ? (
+    <p>manifest data: {manifestData.manifestTrackingNumber}</p>
+  ) : (
+    <p>no manifest data {error ? 'error msg goes here' : ''}</p>
+  );
 }
 
 export default ManifestDetails;
