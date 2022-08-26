2fe46e8e1b69a435eea2ffee03ae0c7bded7f3ab
David Graham <dpgraham4401@gmail.com>
2022-08-26 16:57:54 -0400

    working form example

--- client/src/features/Manifest/ManifestEdit/ManifestEdit.tsx
+++ client/src/features/Manifest/ManifestEdit/ManifestEdit.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Form} from 'react-bootstrap';
 
 function ManifestEdit(): JSX.Element {
-  return <p>Manifest Edit</p>;
+  const [name, setName] = useState('');
+  const [option, setOption] = useState('');
+
+  const onChangeInput = (event: any) => {
+    setName(event.target.value);
+  };
+
+  const onChangeSelect = (event: any) => {
+    setOption(event.target.value);
+  };
+
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(name);
+    console.log(option);
+  };
+
+  return (
+    <div>
+      <Form onSubmit={onSubmit}>
+        {/*<label for="name">Name</label>*/}
+        <input type="text" id="name" name="name" onChange={onChangeInput}/>
+        <select
+          onChange={onChangeSelect}
+          value={option}
+          id="options"
+          name="options"
+        >
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+          <option value="option4">Option4</option>
+        </select>
+      </Form>
+    </div>
+  );
 }
 
 export default ManifestEdit;
