f9dae2d69c4d2c7349c5800b99018c197cb67f88
David Graham <dpgraham4401@gmail.com>
2022-09-15 13:45:35 -0400

    working example with yup, react hook form, and react bootstrap

--- client/src/features/Manifest/Manifest.tsx
+++ client/src/features/Manifest/Manifest.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ManifestDetails from './ManifestDetails/ManifestDetails';
 import ManifestEdit from './ManifestEdit';
+import {Container} from 'react-bootstrap';
 
 function Manifest(): JSX.Element {
   return (
-    <Routes>
-      <Route path=":mtn/view" element={<ManifestDetails />} />
-      <Route path=":mtn/edit" element={<ManifestEdit />} />
-    </Routes>
+    <Container className="py-2">
+      <Routes>
+        <Route path=":mtn/view" element={<ManifestDetails/>}/>
+        <Route path=":mtn/edit" element={<ManifestEdit/>}/>
+      </Routes>
+    </Container>
   );
 }
 
