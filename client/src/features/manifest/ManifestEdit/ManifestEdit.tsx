6fb526f0fa655c94583444703cfcb8b1d448a8ff
David Graham <dpgraham4401@gmail.com>
2022-09-21 16:28:43 -0400

    remove formProvider from HandlerForm and move up one level to ManifestEdit.tsx
    Signed-off-by: David Graham <dpgraham4401@gmail.com>

--- client/src/features/manifest/ManifestEdit/ManifestEdit.tsx
+++ client/src/features/manifest/ManifestEdit/ManifestEdit.tsx
@@ -4,8 +4,7 @@ import { Container, Form } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { HandlerType } from 'types/Handler/Handler';
 import HtCard from 'components/HtCard';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import {useForm, SubmitHandler, FormProvider} from 'react-hook-form';
 
 interface Props {
   manifest?: Manifest;
@@ -18,21 +17,20 @@ type ManifestParams = {
 function ManifestEdit(props: Props): JSX.Element {
   const methods = useForm<Manifest>();
   const {
-    formState: { errors },
+    formState: {errors},
   } = methods;
   const onSubmit: SubmitHandler<Manifest> = (data: Manifest) => {
     console.log(data);
   };
-  const { mtn } = useParams<ManifestParams>();
-  // to be replaced in the future with useEffect to retrieve manifest from server
-  if (mtn?.toUpperCase() === 'NEW' && !props.manifest) {
-    return (
-      <Container>
-        <h2 className="fw-bold">{'Draft Manifest'}</h2>
-        <HtCard id="general-form-card">
-          <HtCard.Header title="General info" />
-          <HtCard.Body>
-            <Form onSubmit={methods.handleSubmit(onSubmit)}>
+  const {mtn} = useParams<ManifestParams>();
+  return (
+    <Container>
+      <FormProvider {...methods}>
+        <Form onSubmit={methods.handleSubmit(onSubmit)}>
+          <h2 className="fw-bold">{'Draft Manifest'}</h2>
+          <HtCard id="general-form-card">
+            <HtCard.Header title="General info"/>
+            <HtCard.Body>
               <Form.Group className="mb-2">
                 <Form.Label className="mb-0">MTN</Form.Label>
                 <Form.Control
@@ -42,30 +40,18 @@ function ManifestEdit(props: Props): JSX.Element {
                   {...methods.register('manifestTrackingNumber')}
                 />
               </Form.Group>
-            </Form>
-          </HtCard.Body>
-        </HtCard>
-        <HtCard id="generator-form-card">
-          <HtCard.Header title="Generator" />
-          <HtCard.Body>
-            <HandlerForm handlerType={HandlerType.Generator} />
-          </HtCard.Body>
-        </HtCard>
-      </Container>
-    );
-  } else {
-    return (
-      <Container>
-        <h2 className="fw-bold">{mtn}</h2>
-        <HtCard id="generator-form-card">
-          <HtCard.Header title="Generator" />
-          <HtCard.Body>
-            <HandlerForm handlerType={HandlerType.Generator} />
-          </HtCard.Body>
-        </HtCard>
-      </Container>
-    );
-  }
+            </HtCard.Body>
+          </HtCard>
+          <HtCard id="generator-form-card">
+            <HtCard.Header title="Generator"/>
+            <HtCard.Body>
+              <HandlerForm handlerType={HandlerType.Generator}/>
+            </HtCard.Body>
+          </HtCard>
+        </Form>
+      </FormProvider>
+    </Container>
+  );
 }
 
 export default ManifestEdit;
