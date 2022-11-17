e64a3928ff259ee9a0f72a778007204d3a99d755
David Graham <dpgraham4401@gmail.com>
2022-11-17 18:07:40 -0500

    poc create option select list from search results

--- client/src/components/ManifestForm/TransporterSearch/TransporterSearchForm.tsx
+++ client/src/components/ManifestForm/TransporterSearch/TransporterSearchForm.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Form, Row, Button, Modal } from 'react-bootstrap';
-import { useForm, useFormContext } from 'react-hook-form';
+// import { useForm, useFormContext } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message';
 import api from 'services';
+import { Transporter } from 'types/Transporter/Transporter';
 
 interface SearchCriteria {
   epaId: string | undefined;
@@ -10,30 +12,44 @@ interface SearchCriteria {
 }
 
 function TransporterSearchForm() {
+  const [tranOptions, setTranOptions] = useState<[Transporter] | undefined>(
+    undefined
+  );
   const onSubmit = (data: any) => console.log(data);
   // The although TransporterSearch is a separate form, it's intended to be
   // used in the context of a ManifestForm so it can modify its values.
-  const manifestForm = useFormContext();
+  // const manifestForm = useFormContext();
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm();
-
   let searchData: SearchCriteria = {
     epaId: watch('epaId'),
     name: watch('name'),
   };
-  if (
-    typeof searchData.epaId === 'string' &&
-    typeof searchData.name === 'string'
-  ) {
-    if (searchData.epaId.length >= 3 || searchData.name.length >= 3) {
-      let test = api.post('trak/transporter/search', searchData);
-      console.log(test);
+
+  useEffect(() => {
+    async function fetchOptions() {
+      if (
+        typeof searchData.epaId === 'string' &&
+        typeof searchData.name === 'string'
+      ) {
+        if (searchData.epaId.length >= 3 || searchData.name.length >= 3) {
+          let transporters = await api.post(
+            'trak/transporter/search',
+            searchData
+          );
+          console.log(transporters);
+          return transporters;
+        }
+      }
     }
-  }
+
+    fetchOptions().then((trans) => setTranOptions(trans));
+  }, [watch('epaId'), watch('name')]);
+
   return (
     <>
       <Form onSubmit={handleSubmit(onSubmit)}>
@@ -59,6 +75,25 @@ function TransporterSearchForm() {
                 />
               </Form.Group>
             </Col>
+          </Row>
+          <Row>
+            <Col>
+              {tranOptions ? (
+                <Form.Select>
+                  {tranOptions.map((option) => {
+                    return (
+                      <option value={option.epaSiteId}>
+                        {' '}
+                        {option.epaSiteId} {' -- '} {option.name}{' '}
+                      </option>
+                    );
+                  })}
+                </Form.Select>
+              ) : (
+                <></>
+              )}
+            </Col>
+            {/*  <AsyncSelect loadOptions={searchTransporters} />*/}
           </Row>
           <Row>
             <ErrorMessage
