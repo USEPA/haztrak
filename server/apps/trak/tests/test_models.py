66bd4c314dcda5dfd094979ae6a44c162b4c02d4
David Paul Graham <dpgraham4401@gmail.com>
2022-12-04 16:47:45 -0500

    test_modal.py addition to trak

--- server/apps/trak/tests/test_models.py
+++ server/apps/trak/tests/test_models.py
@@ -3,16 +3,21 @@ from django.test import TestCase
 from apps.trak.models import Address
 
 
+def create_handler(address: str, st_num: str, city: str, country: str):
+    return Address.objects.create(address1=address, street_number=st_num,
+                                  country=country, city=city)
+
+
+def create_address(address: str, st_num: str, city: str, country: str):
+    return Address.objects.create(address1=address, street_number=st_num,
+                                  country=country, city=city)
+
+
 class AddressModelTests(TestCase):
 
-    @staticmethod
-    def create_address(address: str, st_num: str, city: str, country: str):
-        return Address.objects.create(address1=address, street_number=st_num,
-                                      country=country, city=city)
-
-    def test_model_creation(self):
+    def test_address_creation(self):
         address = 'main st.'
-        w = self.create_address(address=address, st_num='123', country='US',
-                                city='foo')
+        w = create_address(address=address, st_num='123', country='US',
+                           city='foo')
         self.assertTrue(isinstance(w, Address))
         self.assertEqual(w.address1, address)
