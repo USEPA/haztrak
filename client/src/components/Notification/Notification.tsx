6a19bbc2d901be755c3a80bd2d75ecbcbe5e2376
David Paul Graham <dpgraham4401@gmail.com>
2022-12-12 09:17:35 -0500

    have notifications widget  display number of notification

--- client/src/components/Notification/Notification.tsx
+++ client/src/components/Notification/Notification.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Badge, Dropdown } from 'react-bootstrap';
+import { useAppSelector } from 'store';
 
 function Notification() {
+  const selector = useAppSelector((state) => state.notification);
+  const numberAlerts = selector.alert.length;
+  console.log(numberAlerts);
+
   return (
     <div className="mx-3">
       <Dropdown>
@@ -11,7 +16,7 @@ function Notification() {
         >
           <i className="fa-regular fa-envelope fa-lg"></i>
           <Badge pill bg="danger px-1" bsPrefix="badge badge-top-right">
-            9+
+            {numberAlerts < 9 ? `${numberAlerts}` : '9+'}
           </Badge>
           <span className="visually-hidden">unread messages</span>
         </Dropdown.Toggle>
