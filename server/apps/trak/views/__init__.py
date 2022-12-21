2162f2b9efdec8fa124a7f267dcb5e8360f936e3
David Graham <dpgraham4401@gmail.com>
2022-12-21 11:33:28 -0500

    rename file profile to rcra_profile for accuracy

--- server/apps/trak/views/__init__.py
+++ server/apps/trak/views/__init__.py
@@ -1,4 +1,4 @@
 from .handler import HandlerSearch, HandlerView
 from .manifest import ManifestView
-from .profile import RcraProfileView, SyncProfile
+from .rcra_profile import RcraProfileView, SyncProfile
 from .sites import SiteApi, SiteList, SiteManifest
