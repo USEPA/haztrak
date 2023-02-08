2048cfbccc2572665f063ccc81a4e4435277abd2
David Graham <dpgraham4401@gmail.com>
2023-02-07 21:09:03 -0500

    intial core test suite for haztrak exception handler

---
+++ server/apps/core/tests/test_exceptions.py
@@ -0,0 +1,15 @@
+from http import HTTPStatus
+
+from django.http import Http404
+
+from apps.core.exceptions import haztrak_exception_handler
+
+
+class TestHaztrakExecptionHandler:
+
+    def test_http404_to_not_found(self):
+        http404_exec = Http404()
+        context = {}
+        response = haztrak_exception_handler(http404_exec, context)
+        assert response.status_code == HTTPStatus.NOT_FOUND
+        assert 'Not found' in response.data['detail']
