7e27fa5c62598a1be6ee478867858e3e0db8e7c5
David Paul Graham <43794491+dpgraham4401@users.noreply.github.com>
2023-04-18 10:25:38 -0500

    ImportInfo adjustemnts (#438)
    * remove Locality model and replace with charfield with state choices
    
    * remove pull_request_target from release drafter workflow
    
    * make PR template little easier on eyes

--- server/apps/sites/models/__init__.py
+++ server/apps/sites/models/__init__.py
@@ -1,3 +1,3 @@
-from .contact_models import Address, Contact, RcraPhone
+from .contact_models import Address, Contact, RcraPhone, RcraStates
 from .profile_models import RcraProfile, RcraSitePermission
 from .site_models import RcraSite, RcraSiteType, Site
