71a8331dcd41fc51d7d4a064bf147aa8cec08cf6
David Paul Graham <43794491+dpgraham4401@users.noreply.github.com>
2023-05-01 11:15:20 -0400

    Logo and images (#459)
    * add logo to login
    
    * fix HtCard default padding
    
    * add new logo to top nav bar
    
    * replace recycle favicon with new svg
    
    * add haztrak logo to docs homepage

--- client/src/features/login/Login.tsx
+++ client/src/features/login/Login.tsx
@@ -1,14 +1,13 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { HtForm } from 'components/Ht';
+import { HtCard, HtForm } from 'components/Ht';
 import React, { ReactElement, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { login, useAppDispatch, useAppSelector } from 'store';
 import { useNavigate } from 'react-router-dom';
 import { useTitle } from 'hooks';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTruckFast } from '@fortawesome/free-solid-svg-icons';
 import { z } from 'zod';
-import { Form } from 'react-bootstrap';
+import { Col, Container, Form, Row } from 'react-bootstrap';
+import logo from 'assets/haztrak-logos/low-resolution/svg/haztrak-low-resolution-logo-black-on-transparent-background.svg';
 
 const loginSchema = z.object({
   username: z.string().min(1, 'Username Required').min(8),
@@ -46,45 +45,88 @@ export function Login(): ReactElement {
   }
 
   return (
-    <div className="col-md-6 offset-md-3 mt-5">
-      <p className="h1 mb-4 text-center">
-        <FontAwesomeIcon icon={faTruckFast} className="pe-2" />
-        Haztrak
-      </p>
-      <div className="card" id="login-card">
-        <h4 className="card-header bg-primary text-light">Login</h4>
-        <div className="card-body">
-          <HtForm onSubmit={handleSubmit(onSubmit)}>
-            <HtForm.Group>
-              <HtForm.Label htmlFor="username">Username</HtForm.Label>
-              <Form.Control
-                id="username"
-                type="text"
-                placeholder={'wary-walrus-123'}
-                {...register('username', {})}
-                className={errors.username && 'is-invalid'}
-              />
-              <div className="invalid-feedback">{errors.username?.message}</div>
-            </HtForm.Group>
-            <HtForm.Group>
-              <HtForm.Label htmlFor="password">Password</HtForm.Label>
-              <Form.Control
-                id="password"
-                type="password"
-                placeholder="MyP@ssword123"
-                {...register('password', {})}
-                className={errors.password && 'is-invalid'}
-              />
-              <div className="invalid-feedback">{errors.password?.message}</div>
-            </HtForm.Group>
-            <button type="submit" disabled={isSubmitting} className="btn btn-primary m-2">
-              {isSubmitting && <span className="spinner-border spinner-border-sm mr-1" />}
-              Login
-            </button>
-            {authError && <div className="alert alert-danger mt-3 mb-0">{authError}</div>}
-          </HtForm>
-        </div>
-      </div>
-    </div>
+    <Container className="d-flex justify-content-center m-5">
+      <Col className="m-5">
+        <Row className="d-flex justify-content-center">
+          <img
+            src={logo}
+            alt="haztrak logo, hazardous waste tracking made easy."
+            width="auto"
+            height={100}
+            className="my-3"
+          />
+        </Row>
+        <Row>
+          <HtCard>
+            <HtCard.Header title="login" />
+            <HtCard.Body>
+              <HtForm onSubmit={handleSubmit(onSubmit)}>
+                <HtForm.Group>
+                  <HtForm.Label htmlFor="username">Username</HtForm.Label>
+                  <Form.Control
+                    id="username"
+                    type="text"
+                    placeholder={'wary-walrus-123'}
+                    {...register('username', {})}
+                    className={errors.username && 'is-invalid'}
+                  />
+                  <div className="invalid-feedback">{errors.username?.message}</div>
+                </HtForm.Group>
+                <HtForm.Group>
+                  <HtForm.Label htmlFor="password">Password</HtForm.Label>
+                  <Form.Control
+                    id="password"
+                    type="password"
+                    placeholder="MyP@ssword123"
+                    {...register('password', {})}
+                    className={errors.password && 'is-invalid'}
+                  />
+                  <div className="invalid-feedback">{errors.password?.message}</div>
+                </HtForm.Group>
+                <button type="submit" disabled={isSubmitting} className="btn btn-primary m-2">
+                  {isSubmitting && <span className="spinner-border spinner-border-sm mr-1" />}
+                  Login
+                </button>
+                {authError && <div className="alert alert-danger mt-3 mb-0">{authError}</div>}
+              </HtForm>
+            </HtCard.Body>
+          </HtCard>
+          {/*<div className="card" id="login-card">*/}
+          {/*  <h4 className="card-header bg-primary text-light">Login</h4>*/}
+          {/*  <div className="card-body">*/}
+          {/*    <HtForm onSubmit={handleSubmit(onSubmit)}>*/}
+          {/*      <HtForm.Group>*/}
+          {/*        <HtForm.Label htmlFor="username">Username</HtForm.Label>*/}
+          {/*        <Form.Control*/}
+          {/*          id="username"*/}
+          {/*          type="text"*/}
+          {/*          placeholder={'wary-walrus-123'}*/}
+          {/*          {...register('username', {})}*/}
+          {/*          className={errors.username && 'is-invalid'}*/}
+          {/*        />*/}
+          {/*        <div className="invalid-feedback">{errors.username?.message}</div>*/}
+          {/*      </HtForm.Group>*/}
+          {/*      <HtForm.Group>*/}
+          {/*        <HtForm.Label htmlFor="password">Password</HtForm.Label>*/}
+          {/*        <Form.Control*/}
+          {/*          id="password"*/}
+          {/*          type="password"*/}
+          {/*          placeholder="MyP@ssword123"*/}
+          {/*          {...register('password', {})}*/}
+          {/*          className={errors.password && 'is-invalid'}*/}
+          {/*        />*/}
+          {/*        <div className="invalid-feedback">{errors.password?.message}</div>*/}
+          {/*      </HtForm.Group>*/}
+          {/*      <button type="submit" disabled={isSubmitting} className="btn btn-primary m-2">*/}
+          {/*        {isSubmitting && <span className="spinner-border spinner-border-sm mr-1" />}*/}
+          {/*        Login*/}
+          {/*      </button>*/}
+          {/*      {authError && <div className="alert alert-danger mt-3 mb-0">{authError}</div>}*/}
+          {/*    </HtForm>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
+        </Row>
+      </Col>
+    </Container>
   );
 }
