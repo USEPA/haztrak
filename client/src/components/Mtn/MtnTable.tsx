3f61eb6b0ba37874e4ef5e9c96bf5d71623d02cd
David Graham <dpgraham4401@gmail.com>
2023-05-08 09:12:38 -0400

    initial render MTN with react table v8

--- client/src/components/Mtn/MtnTable.tsx
+++ client/src/components/Mtn/MtnTable.tsx
@@ -1,11 +1,11 @@
-import { MtnRowActions } from 'components/Mtn/MtnRowActions';
-import React, { ReactElement, useMemo, useState } from 'react';
-import { HtPaginate, HtTooltip } from 'components/Ht';
-import { Row, Table } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faPen } from '@fortawesome/free-solid-svg-icons';
-import { CellProps, Column, useTable } from 'react-table';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import React from 'react';
+import { Table } from 'react-bootstrap';
 import { z } from 'zod';
 
 const mtnDetailsSchema = z.object({
@@ -15,10 +15,6 @@ const mtnDetailsSchema = z.object({
   status: z.string(),
   actions: z.any(),
 });
-
-const subTypeCell = ({ value }: CellProps<MtnDetails>) => {
-  if (value === 'FullElectronic') return 'Electronic';
-};
 
 /**
  * Select details about a manifest for display, navigation, and analysis.
@@ -31,134 +27,64 @@ interface MtnTableProps {
   pageSize?: number;
 }
 
+const columnHelper = createColumnHelper<MtnDetails>();
+
+const columns = [
+  columnHelper.accessor('manifestTrackingNumber', {
+    cell: (info) => info.getValue(),
+  }),
+];
+
 /**
  * Returns a card with a table of manifest tracking numbers (MTN) and select details
  * @param manifest
  */
-export function MtnTable({ manifests, pageSize = 10 }: MtnTableProps): ReactElement {
-  // @ts-ignore
-  const mtnColumns: Column<MtnDetails>[] = useMemo(
-    () => [
-      {
-        Header: 'MTN',
-        accessor: 'manifestTrackingNumber', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Status',
-        accessor: 'status',
-      },
-      {
-        Header: 'Type',
-        accessor: 'submissionType',
-        Cell: ({ value }: CellProps<MtnDetails>) => {
-          if (value === 'FullElectronic') return 'Electronic';
-          if (value === 'DataImage5Copy') return 'Data + Image';
-          else return value;
-        },
-      },
-      {
-        Header: 'Actions',
-        accessor: 'actions',
-        Cell: ({
-          cell: {
-            row: {
-              values: { manifestTrackingNumber },
-            },
-          },
-        }: CellProps<MtnDetails>) => <MtnRowActions mtn={manifestTrackingNumber} />,
-      },
-    ],
-    []
-  );
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const currentTableData = useMemo(() => {
-    const firstPageIndex = (currentPage - 1) * pageSize;
-    const lastPageIndex = firstPageIndex + pageSize;
-    return manifests.slice(firstPageIndex, lastPageIndex);
-  }, [currentPage, pageSize, manifests]);
-  const { getTableProps, headerGroups, rows, prepareRow, getTableBodyProps } = useTable({
-    columns: mtnColumns,
+export function MtnTable({ manifests }: MtnTableProps) {
+  const table = useReactTable({
+    columns,
     data: manifests,
+    getCoreRowModel: getCoreRowModel(),
   });
-  if (manifests.length === 0) {
-    return (
-      <>
-        <Row className="text-center">
-          <h3>No Manifest Found</h3>
-        </Row>
-      </>
-    );
-  }
 
   return (
     <>
-      <Table hover {...getTableProps()}>
+      <Table>
         <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(header.column.columnDef.header, header.getContext())}
+                </th>
               ))}
             </tr>
           ))}
         </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-                })}
-              </tr>
-            );
-          })}
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+              ))}
+            </tr>
+          ))}
         </tbody>
-        {/*<thead>*/}
-        {/*  <tr>*/}
-        {/*    <th>Manifest Tracking Number</th>*/}
-        {/*    <th>Status</th>*/}
-        {/*    <th className="d-flex justify-content-center">Actions</th>*/}
-        {/*  </tr>*/}
-        {/*</thead>*/}
-        {/*<tbody>*/}
-        {/*  {currentTableData.map(({ manifestTrackingNumber, status }, i) => {*/}
-        {/*    return (*/}
-        {/*      <tr key={`mtn${i}`}>*/}
-        {/*        <td>{manifestTrackingNumber}</td>*/}
-        {/*        <td>{status}</td>*/}
-        {/*        <td>*/}
-        {/*          <div className="d-flex justify-content-evenly">*/}
-        {/*            <HtTooltip text={`View: ${manifestTrackingNumber}`}>*/}
-        {/*              <Link*/}
-        {/*                to={`./${manifestTrackingNumber}/view`}*/}
-        {/*                aria-label={`viewManifest${manifestTrackingNumber}`}*/}
-        {/*              >*/}
-        {/*                <FontAwesomeIcon icon={faEye} />*/}
-        {/*              </Link>*/}
-        {/*            </HtTooltip>*/}
-        {/*            <HtTooltip text={`Edit ${manifestTrackingNumber}`}>*/}
-        {/*              <Link*/}
-        {/*                to={`./${manifestTrackingNumber}/edit`}*/}
-        {/*                aria-label={`editManifest${manifestTrackingNumber}`}*/}
-        {/*              >*/}
-        {/*                <FontAwesomeIcon icon={faPen} />*/}
-        {/*              </Link>*/}
-        {/*            </HtTooltip>*/}
-        {/*          </div>*/}
-        {/*        </td>*/}
-        {/*      </tr>*/}
-        {/*    );*/}
-        {/*  })}*/}
-        {/*</tbody>*/}
+        <tfoot>
+          {table.getFooterGroups().map((footerGroup) => (
+            <tr key={footerGroup.id}>
+              {footerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(header.column.columnDef.footer, header.getContext())}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
       </Table>
-      <HtPaginate
-        currentPage={currentPage}
-        onPageChange={setCurrentPage}
-        totalCount={manifests.length}
-        pageSize={pageSize}
-      />
     </>
   );
 }
