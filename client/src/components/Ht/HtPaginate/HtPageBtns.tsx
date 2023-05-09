dec461602ad99e651e0f52d7ad8af98e004f8f0e
David Graham <dpgraham4401@gmail.com>
2023-05-09 11:25:23 -0400

    repalce buttons with react-bootstrap pagination for accessibility

--- client/src/components/Ht/HtPaginate/HtPageBtns.tsx
+++ client/src/components/Ht/HtPaginate/HtPageBtns.tsx
@@ -7,7 +7,7 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Table } from '@tanstack/react-table';
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Pagination } from 'react-bootstrap';
 import { boolean } from 'zod';
 
 interface HtPageBtnsProps<T> {
@@ -22,54 +22,43 @@ interface HtPageBtnsProps<T> {
  * @constructor
  */
 export function HtPageBtns<T>({ table }: HtPageBtnsProps<T>) {
-  console.log(table.getPageOptions());
   return (
-    <div className="d-flex justify-content-center">
-      <div className="bg-light rounded-5 px-1">
-        <Button
-          className="bg-transparent border-0 text-primary px-2"
-          onClick={() => table.setPageIndex(0)}
-          disabled={!table.getCanPreviousPage()}
-        >
-          <FontAwesomeIcon icon={faBackwardFast} size={'lg'} className="text-primary" />
-        </Button>
-        <Button
-          className="bg-transparent border-0 text-primary px-2"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          <FontAwesomeIcon icon={faCaretLeft} size={'lg'} className="text-primary" />
-        </Button>
-        {table.getPageOptions().map((pageNumber: number) => {
-          const pageIndex = table.getState().pagination.pageIndex;
-          return (
-            <Button
-              className={
-                pageIndex === pageNumber
-                  ? 'bg-primary rounded-circle mx-1 py-1'
-                  : 'bg-transparent rounded-circle text-primary mx-1 py-1'
-              }
-              onClick={() => table.setPageIndex(pageNumber)}
-            >
-              {pageNumber + 1}
-            </Button>
-          );
-        })}
-        <Button
-          className="bg-transparent border-0 text-primary px-2"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          <FontAwesomeIcon icon={faCaretRight} size={'lg'} className="text-primary" />
-        </Button>
-        <Button
-          className="bg-transparent border-0 text-primary px-2"
-          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-          disabled={!table.getCanNextPage()}
-        >
-          <FontAwesomeIcon icon={faForwardFast} size={'lg'} className="text-primary" />
-        </Button>
+    <>
+      <div className="d-flex justify-content-center">
+        <Pagination className="bg-light rounded-5 px-1">
+          <Pagination.First
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <FontAwesomeIcon icon={faBackwardFast} size={'lg'} />
+          </Pagination.First>
+          <Pagination.Prev onClick={table.previousPage} disabled={!table.getCanPreviousPage()}>
+            <FontAwesomeIcon icon={faCaretLeft} size={'lg'} />
+          </Pagination.Prev>
+
+          {table.getPageOptions().map((pageNumber: number) => {
+            const pageIndex = table.getState().pagination.pageIndex;
+            return (
+              <Pagination.Item
+                onClick={() => table.setPageIndex(pageNumber)}
+                key={pageNumber}
+                active={pageNumber === pageIndex}
+              >
+                {pageNumber + 1}
+              </Pagination.Item>
+            );
+          })}
+          <Pagination.Next onClick={table.nextPage} disabled={!table.getCanNextPage()}>
+            <FontAwesomeIcon icon={faCaretRight} size={'lg'} />
+          </Pagination.Next>
+          <Pagination.Last
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            <FontAwesomeIcon icon={faForwardFast} size={'lg'} />
+          </Pagination.Last>
+        </Pagination>
       </div>
-    </div>
+    </>
   );
 }
