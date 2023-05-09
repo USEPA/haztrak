ca1483ec038af53f14d765aa1718d6aaa7196d1e
David Graham <dpgraham4401@gmail.com>
2023-05-09 16:40:45 -0400

    mockMtnDetails object factory

--- client/src/components/Mtn/MtnTable.spec.tsx
+++ client/src/components/Mtn/MtnTable.spec.tsx
@@ -1,25 +1,28 @@
 import '@testing-library/jest-dom';
+import { Manifest } from 'components/Manifest';
 import { MtnTable } from 'components/Mtn';
 import React from 'react';
 import { cleanup, renderWithProviders, screen } from 'test-utils';
 import { MtnDetails } from 'components/Mtn';
 
-const manifestDetail: MtnDetails = {
+const DEFAULT_MTN_DETAILS: MtnDetails = {
   manifestTrackingNumber: '123456789ELC',
   status: 'InTransit',
   submissionType: 'FullElectronic',
   signatureStatus: false,
 };
 
-const mtnData = [manifestDetail, manifestDetail];
-
-afterEach(() => {
-  cleanup();
-});
+export function createMockMtnDetails(overWrites?: Partial<MtnDetails>): MtnDetails {
+  return {
+    ...DEFAULT_MTN_DETAILS,
+    ...overWrites,
+  };
+}
 
 describe('MtnTable', () => {
   test('renders', async () => {
+    const mtnData = [createMockMtnDetails(), createMockMtnDetails()];
     renderWithProviders(<MtnTable manifests={mtnData} />);
-    expect(await screen.findAllByText(manifestDetail.manifestTrackingNumber)).toHaveLength(2);
+    expect(await screen.findAllByText(DEFAULT_MTN_DETAILS.manifestTrackingNumber)).toHaveLength(2);
   });
 });
