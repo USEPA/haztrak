33b39b310432a6381df94fb427bc6bba5a308777
David Paul Graham <43794491+dpgraham4401@users.noreply.github.com>
2023-06-02 15:29:49 -0400

    remove 2nd container in profile feature to fix alignment (#510)

--- client/src/features/profile/Profile.tsx
+++ client/src/features/profile/Profile.tsx
@@ -22,26 +22,26 @@ export function Profile(): ReactElement {
 
   return (
     <>
-      <Container className="py-2">
+      <Container fluid className="py-2">
         <Row className="d-flex justify-content-start">
           <h1 className="fw-bold">Profile</h1>
         </Row>
-      </Container>
-      <Container fluid className="py-3 d-flex justify-content-center">
-        <Col xs={12} md={11} lg={10} xl={8} className="justify-content-center">
-          <HtCard>
-            <HtCard.Header title="User Profile" />
-            <HtCard.Body>
-              <UserProfile profile={profile} />
-            </HtCard.Body>
-          </HtCard>
-          <HtCard>
-            <HtCard.Header title="RCRAInfo Profile" />
-            <HtCard.Body>
-              <RcraProfile profile={profile} />
-            </HtCard.Body>
-          </HtCard>
-        </Col>
+        <Row className="d-flex justify-content-center">
+          <Col xs={12} md={11} lg={10} xl={8} className="justify-content-center">
+            <HtCard>
+              <HtCard.Header title="User Profile" />
+              <HtCard.Body>
+                <UserProfile profile={profile} />
+              </HtCard.Body>
+            </HtCard>
+            <HtCard>
+              <HtCard.Header title="RCRAInfo Profile" />
+              <HtCard.Body>
+                <RcraProfile profile={profile} />
+              </HtCard.Body>
+            </HtCard>
+          </Col>
+        </Row>
       </Container>
     </>
   );
