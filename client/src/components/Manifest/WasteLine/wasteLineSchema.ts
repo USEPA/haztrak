c890d155845db0448a941db7c2b3045000aca3a4
David Graham <dpgraham4401@gmail.com>
2023-06-14 12:34:43 -0400

    repalce WasteLine interface with type infered from zod schema

--- client/src/components/Manifest/WasteLine/wasteLineSchema.ts
+++ client/src/components/Manifest/WasteLine/wasteLineSchema.ts
@@ -33,33 +33,62 @@ const quantitySchema = z.object({
   unitOfMeasurement: quantityUOMSchema,
 });
 
+const codeSchema = z.object({
+  code: z.string(),
+  description: z.string().optional(),
+});
+
+/**
+ * EPA defined generic interface for codes of various types and their description
+ * such as density UOM, waste codes, form codes, and more.
+ */
+export type Code = z.infer<typeof codeSchema>;
+
+const hazardousWasteSchema = z.object({
+  federalWasteCodes: z.array(codeSchema).optional(),
+  tsdfStateWasteCodes: z.array(codeSchema).optional(),
+  txWasteCodes: z.string().optional(),
+  generatorStateWasteCodes: z.array(codeSchema).optional(),
+});
+
 export const wasteLineSchema = z.object({
   lineNumber: z.number(),
   dotHazardous: z.boolean(),
   epaWaste: z.boolean(),
   pcb: z.boolean(),
+  dotInformation: z.any().optional(),
+  wasteDescription: z.string().optional(),
   quantity: quantitySchema,
+  brInfo: z.any().optional(),
+  br: z.boolean(),
+  hazardousWaste: hazardousWasteSchema.optional(),
+  pcbInfos: z.any().optional(),
+  discrepancyResidueInfo: z.any().optional(),
+  managementMethod: z.any().optional(),
+  additionalInfo: z.any().optional(),
 });
 
-/**
- * Represents waste information captures on EPA's hazardous waste manifest
- */
-interface WasteLine {
-  dotHazardous: boolean;
-  epaWaste: boolean;
-  pcb: boolean;
-  lineNumber: number;
-  dotInformation?: DotInformation;
-  wasteDescription?: string;
-  quantity?: Quantity;
-  brInfo?: BrInfo;
-  br: boolean;
-  hazardousWaste?: HazardousWaste;
-  pcbInfos?: PcbInfo[];
-  discrepancyResidueInfo?: DiscrepancyResidueInfo;
-  managementMethod?: Code;
-  additionalInfo?: AdditionalInfo;
-}
+export type WasteLine = z.infer<typeof wasteLineSchema>;
+
+// /**
+//  * Represents waste information captures on EPA's hazardous waste manifest
+//  */
+// interface WasteLine {
+//   dotHazardous: boolean;
+//   epaWaste: boolean;
+//   pcb: boolean;
+//   lineNumber: number;
+//   dotInformation?: DotInformation;
+//   wasteDescription?: string;
+//   quantity?: Quantity;
+//   brInfo?: BrInfo;
+//   br: boolean;
+//   hazardousWaste?: HazardousWaste;
+//   pcbInfos?: PcbInfo[];
+//   discrepancyResidueInfo?: DiscrepancyResidueInfo;
+//   managementMethod?: Code;
+//   additionalInfo?: AdditionalInfo;
+// }
 
 /**
  * United States Department of Transportation (DOT) information
@@ -71,15 +100,6 @@ interface DotInformation {
    * RQ Description, Technical Name, Hazard Class, Packing Group and any user edits
    */
   printedDotInformation: string;
-}
-
-/**
- * EPA defined generic interface for codes of various types and their description
- * such as density UOM, waste codes, form codes, and more.
- */
-export interface Code {
-  code: string;
-  description?: string;
 }
 
 /**
@@ -169,4 +189,4 @@ interface Comment {
   handlerId: string;
 }
 
-export type { WasteLine, ContainerType, ContainerDescription, QuantityDescription };
+export type { ContainerType, ContainerDescription, QuantityDescription };
--- client/src/test-utils/fixtures/mockWaste.ts
+++ client/src/test-utils/fixtures/mockWaste.ts
@@ -3,6 +3,12 @@ import { WasteLine } from 'components/Manifest/WasteLine/wasteLineSchema';
 const DEFAULT_WASTELINE: WasteLine = {
   dotHazardous: false,
   epaWaste: true,
+  quantity: {
+    quantity: 1,
+    unitOfMeasurement: { code: 'P' },
+    containerNumber: 1,
+    containerType: { code: 'BA' },
+  },
   br: false,
   pcb: false,
   lineNumber: 1,
