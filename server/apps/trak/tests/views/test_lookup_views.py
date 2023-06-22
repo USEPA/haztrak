82e054a834aa1fcbc3929d2359998f5294a92dd2
David Paul Graham <dpgraham4401@gmail.com>
2023-06-21 20:58:10 -0400

    add test to show StateWasteCodesView filters state waste codes by state_id

--- server/apps/trak/tests/views/test_lookup_views.py
+++ server/apps/trak/tests/views/test_lookup_views.py
@@ -38,20 +38,23 @@ class TestLookupViews:
         assert len(response.data) == number_federal_codes
 
     def test_state_returns_200(self, factory, user):
+        # Arrange
         state_id = "VA"
         request = factory.get(f"{self.base_url}/waste/state/{state_id}")
         force_authenticate(request, user)
-
+        # Act
         response: Response = StateWasteCodesView.as_view()(request, state_id=state_id)
-
+        # Assert
         assert response.status_code == status.HTTP_200_OK
 
-    #
-    # def test_federal_returns_list_of_all_federal_codes(self, factory, user):
-    #     number_federal_codes = WasteCode.federal.all().count()
-    #     request = factory.get(f"{self.base_url}/waste/federal")
-    #     force_authenticate(request, user)
-    #
-    #     response: Response = FederalWasteCodesView.as_view()(request)
-    #
-    #     assert len(response.data) == number_federal_codes
+    def test_state_waste_codes_returns_list_codes(self, factory, user):
+        # Arrange
+        number_state_codes = WasteCode.state.filter(state_id=WasteCode.VA).count()
+        state_id = "VA"
+        request = factory.get(f"{self.base_url}/waste/state/{state_id}")
+        force_authenticate(request, user)
+        # Act
+        response: Response = StateWasteCodesView.as_view()(request, state_id=state_id)
+        # Assert
+        assert len(response.data) == number_state_codes
+        assert len(response.data) > 0
