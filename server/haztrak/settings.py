055abbc474145157dbfc83a25995db5eac885e48
David Graham <dpgraham4401@gmail.com>
2023-06-29 10:37:38 -0400

    quick fix to allow developers to use the browsable API while logged in

--- server/haztrak/settings.py
+++ server/haztrak/settings.py
@@ -147,6 +147,7 @@ STATICFILES_DIRS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     # 'DEFAULT_PERMISSION_CLASSES': [],  # uncomment to use browser to inspect the API for dev
--- server/haztrak/urls.py
+++ server/haztrak/urls.py
@@ -22,6 +22,7 @@ admin.site.site_header = "Haztrak Admin"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api-auth", include("rest_framework.urls")),
     path(
         "api/",
         include(
