9de60fb3a0d6e4d9dc50f42e5985fa4cbfb5bed6
David Graham <dpgraham4401@gmail.com>
2023-09-25 09:34:32 -0400

    remove un-uneeded file layering in rtk store directory

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,15 +1,11 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { addNotification, removeNotification } from './notificationSlice';
-import { useAppDispatch, useAppSelector } from './hooks';
 import type { AppDispatch, RootState } from './rootStore';
 import { AppStore, login, rootStore, setupStore } from './rootStore';
 
-export {
-  rootStore,
-  login,
-  setupStore,
-  addNotification,
-  removeNotification,
-  useAppSelector,
-  useAppDispatch,
-};
+// TypeSafe redux hooks for using the store
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export { rootStore, login, setupStore, addNotification, removeNotification };
 export type { RootState, AppDispatch, AppStore };
