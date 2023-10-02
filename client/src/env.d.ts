b73855b06d93363f625c77ee4770261efc17245f
David Graham <dpgraham4401@gmail.com>
2023-10-02 11:15:59 -0400

    add rollupOptions to split third party dependencies into separate fils to decrease initial bundle size

--- client/src/env.d.ts
+++ client/src/env.d.ts
@@ -1,6 +1,11 @@
 /// <reference types="vite/client" />
+
+// see Vite's documentation on environment variables
+// https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
+
 interface ImportMetaEnv {
   readonly VITE_HT_API_URL: string;
+  // more env variables...
 }
 
 interface ImportMeta {
