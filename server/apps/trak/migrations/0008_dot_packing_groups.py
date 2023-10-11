b06e2470044308e6a98f1dd43a290009101a9e26
David Graham <dpgraham4401@gmail.com>
2023-10-10 20:12:03 -0400

    add dot hazard classes to data migration

--- server/apps/trak/migrations/0008_dot_packing_groups.py
+++ server/apps/trak/migrations/0008_dot_packing_groups.py
@@ -20,6 +20,70 @@ def populate_dot_packing_groups(apps, schema_editor):
         DotOption.objects.create(**group)
 
 
+def populate_dot_hazard_classes(apps, schema_editor):
+    # WasteCode = apps.get_model("trak", "WasteCode")
+
+    # Replace this with your logic to fetch state waste codes from RCRAInfo
+    packing_groups = [
+        {"value": "I", "value_type": DotOptionType.GROUP},
+        {"value": "II", "value_type": DotOptionType.GROUP},
+        {"value": "III", "value_type": DotOptionType.GROUP},
+        {"value": "3", "value_type": DotOptionType.CLASS},
+        {"value": "5.1", "value_type": DotOptionType.CLASS},
+        {"value": "4.1", "value_type": DotOptionType.CLASS},
+        {"value": "1.5D", "value_type": DotOptionType.CLASS},
+        {"value": "1.1D", "value_type": DotOptionType.CLASS},
+        {"value": "1.2G", "value_type": DotOptionType.CLASS},
+        {"value": "1.3G", "value_type": DotOptionType.CLASS},
+        {"value": "1.4G", "value_type": DotOptionType.CLASS},
+        {"value": "1.3J", "value_type": DotOptionType.CLASS},
+        {"value": "1.2H", "value_type": DotOptionType.CLASS},
+        {"value": "1.3H", "value_type": DotOptionType.CLASS},
+        {"value": "9", "value_type": DotOptionType.CLASS},
+        {"value": "1.2K", "value_type": DotOptionType.CLASS},
+        {"value": "1.3K", "value_type": DotOptionType.CLASS},
+        {"value": "1.6N", "value_type": DotOptionType.CLASS},
+        {"value": "1.4S", "value_type": DotOptionType.CLASS},
+        {"value": "1.4B", "value_type": DotOptionType.CLASS},
+        {"value": "1.4C", "value_type": DotOptionType.CLASS},
+        {"value": "1.4D", "value_type": DotOptionType.CLASS},
+        {"value": "1.1L", "value_type": DotOptionType.CLASS},
+        {"value": "1.2L", "value_type": DotOptionType.CLASS},
+        {"value": "1.3L", "value_type": DotOptionType.CLASS},
+        {"value": "8", "value_type": DotOptionType.CLASS},
+        {"value": "1.1C", "value_type": DotOptionType.CLASS},
+        {"value": "1.1E", "value_type": DotOptionType.CLASS},
+        {"value": "1.1F", "value_type": DotOptionType.CLASS},
+        {"value": "1.2C", "value_type": DotOptionType.CLASS},
+        {"value": "1.2D", "value_type": DotOptionType.CLASS},
+        {"value": "1.2E", "value_type": DotOptionType.CLASS},
+        {"value": "1.2F", "value_type": DotOptionType.CLASS},
+        {"value": "1.3C", "value_type": DotOptionType.CLASS},
+        {"value": "1.4E", "value_type": DotOptionType.CLASS},
+        {"value": "1.4F", "value_type": DotOptionType.CLASS},
+        {"value": "6.1", "value_type": DotOptionType.CLASS},
+        {"value": "1.1G", "value_type": DotOptionType.CLASS},
+        {"value": "1.1A", "value_type": DotOptionType.CLASS},
+        {"value": "6.2", "value_type": DotOptionType.CLASS},
+        {"value": "1.1J", "value_type": DotOptionType.CLASS},
+        {"value": "1.2J", "value_type": DotOptionType.CLASS},
+        {"value": "1.1B", "value_type": DotOptionType.CLASS},
+        {"value": "1.2B", "value_type": DotOptionType.CLASS},
+        {"value": "Comb liq", "value_type": DotOptionType.CLASS},
+        {"value": "5.2", "value_type": DotOptionType.CLASS},
+        {"value": "7", "value_type": DotOptionType.CLASS},
+        {"value": "2.1", "value_type": DotOptionType.CLASS},
+        {"value": "2.2", "value_type": DotOptionType.CLASS},
+        {"value": "ORM-D", "value_type": DotOptionType.CLASS},
+        {"value": "2.3", "value_type": DotOptionType.CLASS},
+        {"value": "4.2", "value_type": DotOptionType.CLASS},
+        {"value": "4.3", "value_type": DotOptionType.CLASS},
+    ]
+
+    for group in packing_groups:
+        DotOption.objects.create(**group)
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("trak", "0007_dot_id_numbers"),
@@ -27,4 +91,5 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(populate_dot_packing_groups),
+        migrations.RunPython(populate_dot_hazard_classes),
     ]
