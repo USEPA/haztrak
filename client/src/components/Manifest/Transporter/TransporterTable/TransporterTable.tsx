73ef83880ad34ca8961fe753e9cf763d3a7dca36
David Graham <dpgraham4401@gmail.com>
2023-10-23 10:33:00 -0400

    add auto-animate package, implement the transporter table as a list of cards with animations instead of table

--- client/src/components/Manifest/Transporter/TransporterTable/TransporterTable.tsx
+++ client/src/components/Manifest/Transporter/TransporterTable/TransporterTable.tsx
@@ -1,13 +1,11 @@
-import { faCircleCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
-import React from 'react';
-import { Table } from 'react-bootstrap';
-import { Manifest } from 'components/Manifest/manifestSchema';
-import { QuickerSignData } from 'components/Manifest/QuickerSign';
-import { TransporterRowActions } from './TransporterRowActions';
-import { UseFieldArrayReturn } from 'react-hook-form';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { QuickerSignModalBtn } from 'components/Manifest/QuickerSign';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 import { Transporter } from 'components/Manifest';
+import { Manifest } from 'components/Manifest/manifestSchema';
+import { QuickerSignData, QuickerSignModalBtn } from 'components/Manifest/QuickerSign';
+import { TransporterRowActions } from 'components/Manifest/Transporter/TransporterTable/TransporterRowActions';
+import React from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
+import { UseFieldArrayReturn } from 'react-hook-form';
 
 interface TransporterTableProps {
   transporters?: Array<Transporter>;
@@ -22,9 +20,12 @@ function TransporterTable({
   readOnly,
   setupSign,
 }: TransporterTableProps) {
+  const [parent, enableAnimations] = useAutoAnimate();
+
   if (!transporters || transporters.length < 1) {
     return <></>;
   }
+
   if (transporters) {
     for (let i = 0; i < transporters?.length; i++) {
       transporters[i].order = i + 1;
@@ -32,53 +33,58 @@ function TransporterTable({
   }
 
   return (
-    <Table striped>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>EPA ID</th>
-          <th>Name</th>
-          <th className="text-center">Signed</th>
-          <th className="text-center">Edit</th>
-        </tr>
-      </thead>
-      <tbody>
+    <>
+      <div ref={parent}>
         {transporters.map((transporter, index) => {
           return (
-            <tr key={index}>
-              <td>{transporter.order}</td>
-              <td>{transporter.epaSiteId}</td>
-              <td>{transporter.name}</td>
-              <td className="text-center">
-                {transporter.signed ? (
-                  <FontAwesomeIcon icon={faCircleCheck} size="lg" className="text-success" />
-                ) : (
-                  <FontAwesomeIcon icon={faCircleXmark} size="lg" className="text-danger" />
-                )}
-              </td>
-              <td>
-                {readOnly ? (
-                  <QuickerSignModalBtn
-                    siteType={'Transporter'}
-                    mtnHandler={transporter}
-                    handleClick={setupSign}
-                    iconOnly={true}
-                    disabled={transporter.signed}
-                  />
-                ) : (
-                  <TransporterRowActions
-                    removeTransporter={arrayFieldMethods.remove}
-                    swapTransporter={arrayFieldMethods.swap}
-                    index={index}
-                    length={transporters?.length}
-                  />
-                )}
-              </td>
-            </tr>
+            <Card key={transporter.epaSiteId} className="mb-2">
+              <Card.Body>
+                <Row>
+                  <Col>
+                    <h5 className="d-inline border-3 me-3">{transporter.order} </h5>
+                    {/*<span>{transporter.epaSiteId} </span>*/}
+                    <span>{transporter.name} </span>
+                  </Col>
+                  <Col xs={3}>
+                    {readOnly ? (
+                      <QuickerSignModalBtn
+                        siteType={'Transporter'}
+                        mtnHandler={transporter}
+                        handleClick={setupSign}
+                        iconOnly={true}
+                        disabled={transporter.signed}
+                      />
+                    ) : (
+                      <TransporterRowActions
+                        removeTransporter={arrayFieldMethods.remove}
+                        swapTransporter={arrayFieldMethods.swap}
+                        index={index}
+                        length={transporters?.length}
+                      />
+                    )}
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+            // <tr key={index}>
+            //   <td>{transporter.order}</td>
+            //   <td>{transporter.epaSiteId}</td>
+            //   <td>{transporter.name}</td>
+            //   <td className="text-center">
+            //     {transporter.signed ? (
+            //       <FontAwesomeIcon icon={faCircleCheck} size="lg" className="text-success" />
+            //     ) : (
+            //       <FontAwesomeIcon icon={faCircleXmark} size="lg" className="text-danger" />
+            //     )}
+            //   </td>
+            //   <td>
+
+            //   </td>
+            // </tr>
           );
         })}
-      </tbody>
-    </Table>
+      </div>
+    </>
   );
 }
 
