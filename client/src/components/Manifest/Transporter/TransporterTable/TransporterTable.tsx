a1ae88254fefc8c9b90708bdd4419c0501bc4d57
David Graham <dpgraham4401@gmail.com>
2023-10-23 12:50:44 -0400

    update test to use new auto-animated transporter array display and mock window.mediaMatch ResizeObserver globally which is not mocked by jsdom

--- client/src/components/Manifest/Transporter/TransporterTable/TransporterTable.tsx
+++ client/src/components/Manifest/Transporter/TransporterTable/TransporterTable.tsx
@@ -4,7 +4,7 @@ import { Manifest } from 'components/Manifest/manifestSchema';
 import { QuickerSignData, QuickerSignModalBtn } from 'components/Manifest/QuickerSign';
 import { TransporterRowActions } from 'components/Manifest/Transporter/TransporterTable/TransporterRowActions';
 import React from 'react';
-import { Accordion, Card, Col, Row, useAccordionButton } from 'react-bootstrap';
+import { Accordion, AccordionButton, Card, Col, Row, useAccordionButton } from 'react-bootstrap';
 import { UseFieldArrayReturn } from 'react-hook-form';
 
 interface TransporterTableProps {
@@ -15,12 +15,15 @@ interface TransporterTableProps {
 }
 
 function CustomToggle({ children, eventKey }: any) {
-  const decoratedOnClick = useAccordionButton(eventKey, () => console.log('totally custom!'));
+  const decoratedOnClick = useAccordionButton(eventKey);
 
   return (
-    <button type="button" style={{ backgroundColor: 'pink' }} onClick={decoratedOnClick}>
-      {children}
-    </button>
+    // <button type="button" style={{ backgroundColor: 'pink' }} onClick={decoratedOnClick}>
+    //   {children}
+    // </button>
+    <div className="mx-1">
+      <AccordionButton onClick={decoratedOnClick} />
+    </div>
   );
 }
 
@@ -47,11 +50,12 @@ function TransporterTable({
       <Accordion ref={parent}>
         {transporters.map((transporter, index) => {
           return (
-            <Card key={transporter.epaSiteId} className="p-3 my-2">
+            <Card key={transporter.epaSiteId} className="py-2 px-4 m-2">
               <Row>
-                <Col>
+                <Col xs={8}>
                   <h5 className="d-inline border-3 me-3">{transporter.order} </h5>
                   <span>{transporter.name} </span>
+                  <span>{transporter.epaSiteId} </span>
                 </Col>
                 <Col xs={3} className="text-end">
                   {readOnly ? (
@@ -73,26 +77,14 @@ function TransporterTable({
                     </>
                   )}
                 </Col>
+                <Col xs={1}>
+                  <CustomToggle eventKey={transporter.epaSiteId}></CustomToggle>
+                </Col>
               </Row>
               <Accordion.Collapse eventKey={transporter.epaSiteId}>
                 <Card.Body>Hello! I'm the body</Card.Body>
               </Accordion.Collapse>
             </Card>
-            // <tr key={index}>
-            //   <td>{transporter.order}</td>
-            //   <td>{transporter.epaSiteId}</td>
-            //   <td>{transporter.name}</td>
-            //   <td className="text-center">
-            //     {transporter.signed ? (
-            //       <FontAwesomeIcon icon={faCircleCheck} size="lg" className="text-success" />
-            //     ) : (
-            //       <FontAwesomeIcon icon={faCircleXmark} size="lg" className="text-danger" />
-            //     )}
-            //   </td>
-            //   <td>
-
-            //   </td>
-            // </tr>
           );
         })}
       </Accordion>
