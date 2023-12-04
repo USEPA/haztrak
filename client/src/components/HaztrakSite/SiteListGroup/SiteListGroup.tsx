b565c83d9e264e3513da91981876403bd3bf864e
David Paul Graham <dpgraham4401@gmail.com>
2023-12-04 16:46:33 -0500

    SiteFilterForm, a form/field used to filter a user's sites from the list of available by either EPA ID or site name. It stores state in the URL as query parameters. It shows a winking robot if no sites are returned.

--- client/src/components/HaztrakSite/SiteListGroup/SiteListGroup.tsx
+++ client/src/components/HaztrakSite/SiteListGroup/SiteListGroup.tsx
@@ -1,17 +1,44 @@
 import { HaztrakSite } from 'components/HaztrakSite/haztrakSiteSchema';
-import { ListGroup } from 'react-bootstrap';
+import { Col, Container, ListGroup, Row } from 'react-bootstrap';
 import { SiteListItem } from 'components/HaztrakSite/SiteListItem/SiteListItem';
+import { SiteFilterForm } from 'components/HaztrakSite/SiteFilter/SiteFilterForm';
+import React, { useState } from 'react';
+import winkingRobot from '/static/robot-wink.jpg';
 
 interface SiteListGroupProps {
   sites: Array<HaztrakSite>;
 }
 
 export function SiteListGroup({ sites }: SiteListGroupProps) {
+  const [filteredSites, setFilteredSites] = useState(sites);
+
   return (
-    <ListGroup numbered as="ol">
-      {sites.map((site) => {
-        return <SiteListItem key={site.handler.epaSiteId} site={site} />;
-      })}
-    </ListGroup>
+    <>
+      <Row className="pb-3 d-flex justify-content-end">
+        <Col xs={8} sm={6} md={4}>
+          <SiteFilterForm sites={sites} setFilteredSites={setFilteredSites} />
+        </Col>
+      </Row>
+      {filteredSites.length === 0 ? (
+        <Container fluid>
+          <Col className="d-flex justify-content-center">
+            <img
+              src={winkingRobot}
+              alt="winking robot"
+              width={200}
+              height={'auto'}
+              className="d-flex justify-content-center"
+            />
+          </Col>
+          <h4 className="text-center">No sites matched that search</h4>
+        </Container>
+      ) : (
+        <ListGroup numbered as="ol">
+          {filteredSites.map((site) => {
+            return <SiteListItem key={site.handler.epaSiteId} site={site} />;
+          })}
+        </ListGroup>
+      )}
+    </>
   );
 }
