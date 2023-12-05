03de274bc17d9fa32129b3d2205d0e8acfdb6aca
David Paul Graham <dpgraham4401@gmail.com>
2023-12-05 14:45:27 -0500

    new fixture for manifest update service for generating validated data (as if it came from JSON) to pass to the model manager save method

--- server/apps/trak/tests/services/test_manifest_services.py
+++ server/apps/trak/tests/services/test_manifest_services.py
@@ -1,9 +1,10 @@
 import pytest
 import pytest_mock
+from django.forms import model_to_dict
 from rest_framework import status
 
 from apps.core.services import RcrainfoService, get_rcrainfo_client
-from apps.trak.models import WasteLine
+from apps.trak.models import Handler, WasteLine
 from apps.trak.serializers import HandlerSerializer, WasteLineSerializer
 from apps.trak.services import ManifestService, update_manifest
 
@@ -38,6 +39,16 @@ class TestManifestService:
 
 
 class TestUpdateManifest:
+    @pytest.fixture
+    def validated_data_factory(self):
+        def _create_data_dict(*, instance, serializer):
+            data = serializer(instance).data
+            new_serializer = serializer(data=data)
+            new_serializer.is_valid(raise_exception=True)
+            return new_serializer.validated_data
+
+        return _create_data_dict
+
     def test_updates_the_manifest_tracking_number(self, manifest_factory):
         old_mtn = "000000123DFT"
         new_mtn = "000000123DFT"
@@ -46,35 +57,54 @@ class TestUpdateManifest:
         assert new_manifest.mtn == new_mtn
 
     def test_updates_generator(
-        self, manifest_factory, manifest_handler_factory, rcra_site_factory
+        self, manifest_factory, manifest_handler_factory, rcra_site_factory, validated_data_factory
     ):
         mtn = "000000123DFT"
         generator_epa_id = "TXD987654321"
         manifest_factory(mtn=mtn)
         txd987654321 = rcra_site_factory(epa_id=generator_epa_id)
         manifest_generator = manifest_handler_factory(rcra_site=txd987654321)
-        json_data = HandlerSerializer(manifest_generator).data
-        new_serializer = HandlerSerializer(data=json_data)
-        new_serializer.is_valid(raise_exception=True)
-        new_manifest = update_manifest(mtn=mtn, data={"generator": new_serializer.validated_data})
+        generator_validated_data = validated_data_factory(
+            instance=manifest_generator, serializer=HandlerSerializer
+        )
+        new_manifest = update_manifest(mtn=mtn, data={"generator": generator_validated_data})
         assert new_manifest.generator.rcra_site.epa_id == generator_epa_id
 
     def test_removes_old_waste_lines(
-        self, manifest_factory, manifest_handler_factory, rcra_site_factory, waste_line_factory
+        self,
+        manifest_factory,
+        manifest_handler_factory,
+        rcra_site_factory,
+        waste_line_factory,
+        validated_data_factory,
     ):
         mtn = "000000123DFT"
         original_manifest = manifest_factory(mtn=mtn)
         waste_line_factory(manifest=original_manifest, line_number=1)
         new_waste_line = waste_line_factory(manifest=original_manifest, line_number=1)
-        new_waste_data = WasteLineSerializer(data=WasteLineSerializer(new_waste_line).data)
-        new_waste_data.is_valid(raise_exception=True)
+        new_waste_validated_data = validated_data_factory(
+            instance=new_waste_line, serializer=WasteLineSerializer
+        )
         update_manifest(
             mtn=mtn,
-            data={"wastes": [new_waste_data.validated_data]},
+            data={"wastes": [new_waste_validated_data]},
         )
         new_waste_lines = WasteLine.objects.filter(manifest__mtn=mtn)
         for wl in new_waste_lines:
             print(wl)
+
+    def test_updates_tsdf(
+        self, manifest_factory, manifest_handler_factory, rcra_site_factory, validated_data_factory
+    ):
+        mtn = "000000123DFT"
+        tsdf_epa_id = "TXD987654321"
+        manifest_factory(mtn=mtn)
+        txd987654321 = rcra_site_factory(epa_id=tsdf_epa_id)
+        manifest_tsdf = manifest_handler_factory(rcra_site=txd987654321)
+        validated_data = validated_data_factory(
+            instance=manifest_tsdf, serializer=HandlerSerializer
+        )
+        print(validated_data)
 
 
 class TestSignManifest:
