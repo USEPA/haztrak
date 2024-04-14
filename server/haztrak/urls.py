788013992f44d2663da5a494e5300d2a06522111
David Graham <dpgraham4401@gmail.com>
2024-04-14 14:43:01 -0400

    fix login for DRF browsable API, add dev fixture for trak user access to Generators Organzation llc.

--- server/haztrak/urls.py
+++ server/haztrak/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the 'include()' function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 # Change the Django Admin page title
@@ -22,6 +23,7 @@ admin.site.site_header = "Haztrak Admin"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    re_path(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path(
         "api/",
         include(
