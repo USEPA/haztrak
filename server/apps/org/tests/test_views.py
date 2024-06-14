a1a1147a0c290fef2bc49a4af0e839ece56f17eb
David Graham <dpgraham4401@gmail.com>
2024-06-14 17:16:11 -0400

    refactor org app to use namespaces and named endpoints

--- server/apps/org/tests/test_views.py
+++ server/apps/org/tests/test_views.py
@@ -1,21 +1,37 @@
+from unittest.mock import patch
+
+import pytest
 from rest_framework import status
+from rest_framework.reverse import reverse
 from rest_framework.test import APIRequestFactory, force_authenticate
 
+from apps.org.models import Org
 from apps.org.views import OrgDetailsView
 
 
 class TestOrgDetailsView:
-    base_url = "/api/org"
     factory = APIRequestFactory()
 
-    def test_get_returns_org_details(self, org_factory, org_access_factory, user_factory):
-        org = org_factory()
-        user = user_factory()
+    @pytest.fixture
+    def user(self, user_factory):
+        return user_factory()
+
+    @pytest.fixture
+    def org(self, org_factory):
+        return org_factory()
+
+    def test_get_returns_org_details(self, org, org_access_factory, user):
         org_access_factory(org=org, user=user)
-        request = self.factory.get(
-            f"{self.base_url}/{org.id}",
-        )
+        request = self.factory.get(reverse("org:details", args=[org.id]))
         force_authenticate(request, user)
         response = OrgDetailsView.as_view()(request, org_id=org.id)
         assert response.status_code == status.HTTP_200_OK
         assert response.data["id"] == org.id
+
+    def test_404_when_org_id_not_defined(self, org, org_access_factory, user):
+        request = self.factory.get(reverse("org:details", args=["foo"]))
+        force_authenticate(request, user)
+        with patch("apps.org.views.get_org_by_id") as mock_org:
+            mock_org.side_effect = Org.DoesNotExist
+            response = OrgDetailsView.as_view()(request, org_id="foo")
+            assert response.status_code == status.HTTP_404_NOT_FOUND
--- server/apps/org/urls.py
+++ server/apps/org/urls.py
@@ -4,5 +4,5 @@ from apps.org.views import OrgDetailsView
 
 app_name = "org"
 urlpatterns = [
-    path("org/<str:org_id>", OrgDetailsView.as_view()),
+    path("org/<str:org_id>", OrgDetailsView.as_view(), name="details"),
 ]
