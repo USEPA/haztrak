02c04fa58ec62fdbbc86111dda4fee4daafb87a4
David Paul Graham <dpgraham4401@gmail.com>
2024-08-04 09:44:34 -0400

    add select componeent to OrgSelect

--- client/app/components/Org/OrgSelect.spec.tsx
+++ client/app/components/Org/OrgSelect.spec.tsx
@@ -1,11 +1,22 @@
-import { describe, expect, it } from 'vitest';
+import { afterAll, afterEach, beforeAll, describe, expect, it } from 'vitest';
 import { OrgSelect } from '~/components/Org/OrgSelect';
-import { renderWithProviders } from '~/mocks';
+import { cleanup, renderWithProviders } from '~/mocks';
 import { screen } from '@testing-library/react';
+import { setupServer } from 'msw/node';
+import { mockSiteEndpoints } from '~/mocks/handlers';
+
+const server = setupServer(...mockSiteEndpoints);
+afterEach(() => cleanup());
+beforeAll(() => server.listen());
+afterAll(() => server.close()); // Disable API mocking after the tests are done.
 
 describe('OrgSelect Component', () => {
   it('renders', () => {
     renderWithProviders(<OrgSelect />);
-    expect(screen.getByText('hello')).toBeInTheDocument();
+    expect(screen.getByTestId('org-select')).toBeInTheDocument();
+  });
+  it('displays a select component', () => {
+    renderWithProviders(<OrgSelect />);
+    expect(screen.getByRole('combobox')).toBeInTheDocument();
   });
 });
--- client/app/components/Org/OrgSelect.tsx
+++ client/app/components/Org/OrgSelect.tsx
@@ -1,7 +1,15 @@
+import Select from 'react-select';
+import React from 'react';
+
 export const OrgSelect = () => {
   return (
-    <div>
-      <p>hello</p>
+    <div data-testid="org-select">
+      <Select
+        options={[]}
+        classNames={{
+          control: () => `form-control p-0 rounded-2'} `,
+        }}
+      />
     </div>
   );
 };
