6f944900954eeda25c4d839137f0ea27b765f6dc
David Paul Graham <dpgraham4401@gmail.com>
2024-08-15 17:35:30 -0400

    WasteRowActions test file

---
+++ client/app/components/Manifest/WasteLine/WasteLineTable/WasteRowActions.spec.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react';
+import { WasteRowActions } from '~/components/Manifest/WasteLine/WasteLineTable/WasteRowActions';
+import { describe, expect, it, vi } from 'vitest';
+import { UseFieldArrayReturn } from 'react-hook-form';
+import { Manifest } from '~/components/Manifest';
+import userEvent from '@testing-library/user-event';
+
+describe('WasteRowActions', () => {
+  // @ts-expect-error - mock object
+  const mockWasteForm: UseFieldArrayReturn<Manifest, 'wastes'> = {
+    remove: vi.fn(),
+  };
+
+  const mockSetEditWasteLine = vi.fn();
+  const mockToggleWLModal = vi.fn();
+
+  const defaultProps = {
+    index: 0,
+    wasteForm: mockWasteForm,
+    setEditWasteLine: mockSetEditWasteLine,
+    toggleWLModal: mockToggleWLModal,
+    eventKey: '0',
+  };
+
+  it('renders dropdown with actions', () => {
+    render(<WasteRowActions {...defaultProps} />);
+    expect(screen.getByTitle('transporter 1 actions')).toBeInTheDocument();
+  });
+
+  it('calls wasteForm.remove when Remove is clicked', async () => {
+    const user = userEvent.setup();
+    render(<WasteRowActions {...defaultProps} />);
+    await user.click(screen.getByTitle('transporter 1 actions'));
+    await user.click(screen.getByTitle('remove waste line 0'));
+    expect(mockWasteForm.remove).toHaveBeenCalledWith(0);
+  });
+
+  it('calls setEditWasteLine and toggleWLModal when Edit is clicked', async () => {
+    const user = userEvent.setup();
+    render(<WasteRowActions {...defaultProps} />);
+    await user.click(screen.getByTitle('transporter 1 actions'));
+    await user.click(screen.getByTitle('edit waste line 0'));
+    expect(mockSetEditWasteLine).toHaveBeenCalled();
+    expect(mockToggleWLModal).toHaveBeenCalled();
+  });
+
+  it('toggles details view when Details/Close is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(<WasteRowActions {...defaultProps} />);
+
+    await user.click(screen.getByTitle('transporter 1 actions'));
+    await user.click(screen.getByTitle('view waste line 0 details'));
+    expect(screen.getByTitle('view waste line 0 details')).toHaveTextContent('Close');
+    await user.click(screen.getByTitle('view waste line 0 details'));
+    expect(screen.getByTitle('view waste line 0 details')).toHaveTextContent('Details');
+  });
+});
