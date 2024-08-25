ec5a98c9ba807d274132ea480073ed1135665dcf
David Paul Graham <43794491+dpgraham4401@users.noreply.github.com>
2024-08-25 12:10:31 -0500

    Reverse proxy updates (#780)
    * update traefik to use dynamic configs instead of static traefik.toml
    
    * remove static traefik file
    
    * update vite config
    
    * remove docker compose health checks
    
    * remove references to localhost outside configs

--- server/haztrak/settings/dev.py
+++ server/haztrak/settings/dev.py
@@ -5,7 +5,7 @@ from .base import *  # noqa: F403
 # General
 SECRET_KEY = "django-insecure-%btjqoun@6ps$e@8bw$48s+!x1e4aiz&5p2nrf6cmiw4)jsx5d"
 DEBUG = True
-CORS_ORIGIN_WHITELIST = [os.getenv("HT_CORS_DOMAIN", "http://localhost:3000")]
+CORS_ORIGIN_WHITELIST = [os.getenv("HT_CORS_DOMAIN", "http://localhost")]
 
 REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [  # noqa: F405
     "rest_framework.renderers.JSONRenderer",
