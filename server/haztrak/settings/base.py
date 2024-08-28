140118ca163e18596f545a978e85048fddf06d92
David Paul Graham <dpgraham4401@gmail.com>
2024-08-28 14:40:37 -0400

    update settings and urlconfig to serve media in development

--- server/haztrak/settings/base.py
+++ server/haztrak/settings/base.py
@@ -137,8 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
