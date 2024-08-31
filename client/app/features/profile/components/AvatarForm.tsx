96008e1b4da0139368b8f35173f4016e7eb933b7
David Graham <dpgraham4401@gmail.com>
2024-08-30 21:10:39 -0400

    POC uploading file to server

--- client/app/features/profile/components/AvatarForm.tsx
+++ client/app/features/profile/components/AvatarForm.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Input } from '~/components/ui';
+import { useAuth } from '~/hooks';
+import { htApi } from '~/services';
 
 export function AvatarForm() {
   const { register, handleSubmit } = useForm();
+  const { user } = useAuth();
 
   const onSubmit = (data: any) => {
-    console.log('data ', data);
+    const formData = new FormData();
+    formData.append('avatar', data.avatar[0]);
+    console.log('data ', formData);
+    htApi
+      .patch(`/profile/${user?.id}`, formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
 
   return (
