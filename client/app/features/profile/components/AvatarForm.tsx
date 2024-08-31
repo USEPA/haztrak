6d32185244b5bad45f177f256f934cdb44d6f1bd
David Graham <dpgraham4401@gmail.com>
2024-08-30 20:46:45 -0400

    implement simplest exmaple of react hook form and using uncontrolled Input to capture file for upload

--- client/app/features/profile/components/AvatarForm.tsx
+++ client/app/features/profile/components/AvatarForm.tsx
@@ -1,79 +1,18 @@
-import React, { createRef, useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
-import { FaUser } from 'react-icons/fa';
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-  Form,
-  FormControl,
-  FormDescription,
-  FormItem,
-  FormLabel,
-  FormMessage,
-  Input,
-} from '~/components/ui';
-import { useAuth } from '~/hooks';
-import { useUpdateProfileMutation } from '~/store';
+import { Input } from '~/components/ui';
 
-interface AvatarFormProps {
-  avatar?: string;
-}
-
-export function AvatarForm({ avatar }: AvatarFormProps) {
-  const [preview, setPreview] = useState(avatar);
-  const { user } = useAuth();
-  const fileRef = createRef<HTMLInputElement>();
-  const [updateProfile] = useUpdateProfileMutation();
-
-  const form = useForm<{ avatar?: string }>({ values: { avatar } });
-
-  const handleUploadedFile = (event: React.FormEvent<HTMLInputElement>) => {
-    if (!event.currentTarget.files) {
-      return;
-    }
-    const file = event.currentTarget.files[0];
-    const urlImage = URL.createObjectURL(file);
-    setPreview(urlImage);
-    // @ts-expect-error - TODO: fix this
-    updateProfile({ id: user?.id, profile: { avatar: urlImage } });
-  };
+export function AvatarForm() {
+  const { register, handleSubmit } = useForm();
 
   const onSubmit = (data: any) => {
     console.log('data ', data);
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormItem>
-          <FormLabel />
-          <FormControl>
-            <div className="tw-flex tw-justify-center">
-              <button onClick={() => fileRef.current?.click()}>
-                <Input
-                  {...form.register('avatar')}
-                  id="picture"
-                  type="file"
-                  accept="image/png,image/jpeg"
-                  aria-label="Profile Picture"
-                  ref={fileRef}
-                  onChange={handleUploadedFile}
-                  className="tw-hidden"
-                />
-                <Avatar className="tw-h-40 tw-w-40">
-                  <AvatarImage src={preview} alt="avatar" />
-                  <AvatarFallback>
-                    <FaUser size={80} />
-                  </AvatarFallback>
-                </Avatar>
-              </button>
-            </div>
-          </FormControl>
-          <FormDescription />
-          <FormMessage />
-        </FormItem>
-      </form>
-    </Form>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input type="file" {...register('avatar')} />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
