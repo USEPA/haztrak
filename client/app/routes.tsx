bf0f019dac02bec2245609e13bc77b5b79ff03a1
David Graham <dpgraham4401@gmail.com>
2025-01-04 21:40:20 -0500

    add v7 relative splat path future flag

--- client/app/routes.tsx
+++ client/app/routes.tsx
@@ -3,92 +3,99 @@ import { ErrorPage } from '~/features/errorPage/ErrorPage';
 import { Login } from '~/features/login';
 import { Component as PrivateRoute } from '~/features/privateRoute';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <PrivateRoute />,
+      children: [
+        {
+          path: '',
+          lazy: () => import('~/features/layout'),
+          children: [
+            {
+              path: 'organization',
+              lazy: () => import('~/features/org'),
+            },
+            {
+              path: '',
+              lazy: () => import('~/features/dashboard'),
+            },
+            {
+              path: 'profile',
+              lazy: () => import('~/features/profile'),
+            },
+            {
+              path: 'site',
+              children: [
+                {
+                  path: '',
+                  lazy: () => import('~/features/siteList'),
+                },
+                {
+                  path: ':siteId',
+                  lazy: () => import('~/features/siteDetails'),
+                },
+                {
+                  path: ':siteId/manifest',
+                  children: [
+                    {
+                      path: '',
+                      lazy: () => import('~/features/manifestList'),
+                    },
+                    {
+                      path: 'new',
+                      lazy: () => import('~/features/newManifest'),
+                    },
+                    {
+                      path: ':mtn/:action',
+                      lazy: () => import('~/features/manifestDetails'),
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              path: 'manifest',
+              children: [
+                {
+                  path: '',
+                  lazy: () => import('~/features/manifestList'),
+                },
+                {
+                  path: 'new',
+                  lazy: () => import('~/features/newManifest'),
+                },
+                {
+                  path: ':mtn/:action',
+                  lazy: () => import('~/features/manifestDetails'),
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: 'login',
+      element: <Login />,
+    },
+    {
+      path: 'register',
+      lazy: () => import('~/features/register'),
+    },
+    {
+      path: 'about',
+      lazy: () => import('~/features/about'),
+    },
+    {
+      path: '*',
+      element: <ErrorPage code={404} />,
+    },
+  ],
   {
-    path: '/',
-    element: <PrivateRoute />,
-    children: [
-      {
-        path: '',
-        lazy: () => import('~/features/layout'),
-        children: [
-          {
-            path: 'organization',
-            lazy: () => import('~/features/org'),
-          },
-          {
-            path: '',
-            lazy: () => import('~/features/dashboard'),
-          },
-          {
-            path: 'profile',
-            lazy: () => import('~/features/profile'),
-          },
-          {
-            path: 'site',
-            children: [
-              {
-                path: '',
-                lazy: () => import('~/features/siteList'),
-              },
-              {
-                path: ':siteId',
-                lazy: () => import('~/features/siteDetails'),
-              },
-              {
-                path: ':siteId/manifest',
-                children: [
-                  {
-                    path: '',
-                    lazy: () => import('~/features/manifestList'),
-                  },
-                  {
-                    path: 'new',
-                    lazy: () => import('~/features/newManifest'),
-                  },
-                  {
-                    path: ':mtn/:action',
-                    lazy: () => import('~/features/manifestDetails'),
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            path: 'manifest',
-            children: [
-              {
-                path: '',
-                lazy: () => import('~/features/manifestList'),
-              },
-              {
-                path: 'new',
-                lazy: () => import('~/features/newManifest'),
-              },
-              {
-                path: ':mtn/:action',
-                lazy: () => import('~/features/manifestDetails'),
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: 'login',
-    element: <Login />,
-  },
-  {
-    path: 'register',
-    lazy: () => import('~/features/register'),
-  },
-  {
-    path: 'about',
-    lazy: () => import('~/features/about'),
-  },
-  {
-    path: '*',
-    element: <ErrorPage code={404} />,
-  },
-]);
+    future: {
+      v7_relativeSplatPath: true,
+    },
+  }
+);
