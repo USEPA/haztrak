99b85084a0b3bc8a57e9ed9636ed0e8d3971707f
David Graham <dpgraham4401@gmail.com>
2025-01-04 22:25:23 -0500

    remove old tests

--- client/app/features/siteDetails/SiteDetails.spec.tsx
+++ client/app/features/siteDetails/SiteDetails.spec.tsx
@@ -1,9 +1,9 @@
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { setupServer } from 'msw/node';
-import { SiteDetails } from './SiteDetails';
 import { afterAll, afterEach, beforeAll, describe, expect, it, vi } from 'vitest';
-import { mockSiteEndpoints } from '~/mocks/handlers';
 import { renderWithProviders } from '~/mocks';
+import { mockSiteEndpoints } from '~/mocks/handlers';
+import { SiteDetails } from './SiteDetails';
 
 const server = setupServer(...mockSiteEndpoints);
 beforeAll(() => server.listen());
@@ -11,7 +11,7 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 vi.mock('react-router-dom', async (importOriginal) => ({
-  ...(await importOriginal<typeof import('react-router-dom')>()),
+  ...(await importOriginal<typeof import('react-router')>()),
   useNavigate: () => vi.fn(),
   useParams: () => ({ siteId: 'test-site' }),
 }));
@@ -21,14 +21,5 @@ describe('SiteDetails', () => {
     renderWithProviders(<SiteDetails />);
 
     expect(screen.getByTestId('spinner')).toBeInTheDocument();
-  });
-
-  it('renders site details when data is available', async () => {
-    renderWithProviders(<SiteDetails />);
-
-    await waitFor(() => expect(screen.getByTestId(/spinner/i)).toBeInTheDocument());
-    await waitFor(() => expect(screen.queryByTestId(/spinner/i)).not.toBeInTheDocument());
-
-    expect(screen.getAllByText('test-site')[0]).toBeInTheDocument();
   });
 });
