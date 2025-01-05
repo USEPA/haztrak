e1a2ae0f7324a4f2fe8e3f6423090458f97aa230
David Graham <dpgraham4401@gmail.com>
2025-01-04 21:56:37 -0500

    add v7 partial hydration future flag

--- client/app/routes.tsx
+++ client/app/routes.tsx
@@ -98,6 +98,7 @@ export const router = createBrowserRouter(
       v7_relativeSplatPath: true,
       v7_fetcherPersist: true,
       v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
     },
   }
 );
