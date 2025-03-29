76ad4d78ed8c640b38fef60e8bd9f429a66d186c
David Paul Graham <43794491+dpgraham4401@users.noreply.github.com>
2025-03-28 21:28:02 -0400

    Docker uv migration (#828)
    * baseline get local development docker compose up and running again after migration to uv

--- server/run.sh
+++ server/run.sh
@@ -2,4 +2,4 @@
 # Used to start the Production Django server
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn --bind 0.0.0.0:8000 --workers 3 haztrak.wsgi:application
+uv run gunicorn --bind 0.0.0.0:8000 --workers 3 haztrak.wsgi:application
