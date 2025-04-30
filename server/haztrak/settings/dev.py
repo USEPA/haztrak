0e58773ab1aea0a48b718dd5bbfd1fa0233c1f5d
David Paul Graham <dpgraham4401@gmail.com>
2025-04-30 09:37:26 -0400

    use volumes for celery and remove automatic pkill command to kill existing processes which we do not have in our existing multistage docker build

--- server/haztrak/settings/dev.py
+++ server/haztrak/settings/dev.py
@@ -7,6 +7,7 @@ from .base import *  # noqa: F403, S105, RUF100
 # General
 SECRET_KEY = "django-insecure-%btjqoun@6ps$e@8bw$48s+!x1e4aiz&5p2nrf6cmiw4)jsx5d"  # noqa: S105
 DEBUG = True
+
 CORS_ORIGIN_WHITELIST = [os.getenv("HT_CORS_DOMAIN", "http://localhost:3000")]
 
 REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [  # noqa: F405
