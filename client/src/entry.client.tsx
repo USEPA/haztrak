4baf3e5b3e9afde8b26add41f7b4d50d2045d4a3
David Paul Graham <dpgraham4401@gmail.com>
2025-05-01 20:58:15 -0400

    minimal working example with hello world catch all

--- client/src/entry.client.tsx
+++ client/src/entry.client.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { HydratedRouter } from 'react-router/dom';
 
-// Start mock service worker in development mode
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'test') {
-    return;
-  }
-  const { worker } = await import('./mocks/browser');
-  return worker.start();
-}
-
-const container = document.getElementById('root') as HTMLElement;
-const root = createRoot(container);
-
-enableMocking().then(() => {
-  root.render(
-    <React.StrictMode>
-      <HydratedRouter />
-    </React.StrictMode>
-  );
-});
+ReactDOM.hydrateRoot(
+  document,
+  <React.StrictMode>
+    <HydratedRouter />
+  </React.StrictMode>
+);
