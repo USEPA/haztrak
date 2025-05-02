9403c11d82b3b19ddfbb89343a854fc2a27b68e3
David Paul Graham <dpgraham4401@gmail.com>
2025-05-01 22:28:26 -0400

    add additional of our pages back to the route config

--- client/src/features/org/index.ts
+++ client/src/features/org/index.ts
@@ -2,4 +2,3 @@ import { Org, orgLoader } from './Org';
 
 export default Org;
 export { Org as Component };
-export { orgLoader as loader };
--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,8 +1,16 @@
-import { type RouteConfig, index, layout, route } from '@react-router/dev/routes';
+import { type RouteConfig, index, layout, prefix, route } from '@react-router/dev/routes';
 
 export default [
   route('/', './features/privateRoute/index.ts', [
-    layout('./features/layout/index.ts', [index('./features/dashboard/index.ts')]),
+    layout('./features/layout/index.ts', [
+      index('./features/dashboard/index.ts'),
+      route('/profile', './features/profile/index.ts'),
+      route('/organization', './features/org/index.ts'),
+      ...prefix('/site', [
+        index('./features/siteList/index.ts'),
+        route('/:siteId', './features/siteDetails/index.ts'),
+      ]),
+    ]),
   ]),
   route('login', './features/login/index.ts'),
   route('register', './features/register/index.ts'),
