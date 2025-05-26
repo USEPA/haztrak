ad799c41eef59900d691fdc9ae3884e2d67cedba
David Graham <dpgraham4401@gmail.com>
2025-05-26 13:46:16 -0400

    minor type changes for existing router

--- client/app/features/org/Org.spec.tsx
+++ client/app/features/org/Org.spec.tsx
@@ -17,7 +17,7 @@ describe('Org feature', () => {
   describe('org loader function', () => {
     it('returns null when org query parameter is missing', async () => {
       const request = new Request('http://localhost/org');
-      const result = await orgLoader({ request, params: {} });
+      const result = await orgLoader({ request, params: {}, context: {} });
       expect(result).toBeNull();
     });
   });
