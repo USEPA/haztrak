944ab22a9f2d3beedd63bae07d4bea65108e13b0
David Graham <dpgraham4401@gmail.com>
2025-08-12 20:08:30 -0400

    rename rcrainfo_service.py to rcrainfo_client.py

--- server/core/services/__init__.py
+++ server/core/services/__init__.py
@@ -1,4 +1,4 @@
-from .rcrainfo_service import RcraClient, get_rcra_client
+from .rcrainfo_client import RcraClient, get_rcra_client
 from .task_service import TaskService, get_task_status, launch_example_task
 
 __all__ = [
