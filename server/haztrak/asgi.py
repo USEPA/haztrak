b8bb54d16189045ddbd8aeea547f3d275389eb14
David Graham <dpgraham4401@gmail.com>
2025-08-17 15:04:51 -0400

    rename the wsgi and asgi application to just 'app'

--- server/haztrak/asgi.py
+++ server/haztrak/asgi.py
@@ -13,4 +13,4 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "haztrak.settings")
 
-application = get_asgi_application()
+app = get_asgi_application()
