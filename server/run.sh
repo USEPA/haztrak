c8b2eba7b8c4638692aee9e1fc14b42774ad2680
David Graham <dpgraham4401@gmail.com>
2025-08-17 15:03:51 -0400

    remove run.sh shell script

--- server/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Used to start the Production Django server
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-uv run gunicorn --bind 0.0.0.0:8000 --workers 3 haztrak.wsgi:application
